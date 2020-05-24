714106b9071858895f17ff95a2c2220b18d78e75
양기현 <rlgusdid@naver.com>
2020-05-25 00:31:45 +0900

    [baekjoon] 연구소

--- baekjoon/14502.cpp
+++ baekjoon/14502.cpp
@@ -3,93 +3,73 @@
 
 using namespace std;
 
-vector<pair<int,int>> v1;       // 좌표 저장
-vector<int> v2;                 // 좌표의 인덱스 저장
-
-int arr[10][10];
-int tmp[10][10];
 int n,m;
-
+int arr[15][15], tmp_arr[15][15];
+bool visited[15][15];
 int dy[]={0,0,1,-1};
 int dx[]={1,-1,0,0};
-int visited[10][10];
-queue<pair<int,int>> q;
-int mx=-1;
+queue<pair<int,int>> virus;
+vector<pair<int,int>> v1;
+vector<int> v2;
+int t_cnt;
+int mx=-987654321;
 
-void bfs(int y,int x) {
-    visited[y][x]=1;
-    q.push({y,x});
-
-    while(!q.empty()) {
-        tie(y,x)=q.front();
-        q.pop();
+int bfs() {
+    fill(&visited[0][0],&visited[0][0]+15*15,0);
+    queue<pair<int,int>> tmp=virus;
+    
+    int cnt=0;
+    while(!tmp.empty()) {
+        int y=tmp.front().first,x=tmp.front().second;
+        tmp.pop();
 
         for(int i=0;i<4;i++) {
-            int ny=y+dy[i], nx=x+dx[i];
+            int ny=y+dy[i],nx=x+dx[i];
 
-            if(ny<0 || nx<0 || ny>=n || nx>=m) continue;
-
-            if(!visited[ny][nx] && tmp[ny][nx]==0) {
-                visited[ny][nx]=visited[y][x]+1;
-                q.push({ny,nx});
-                tmp[ny][nx]=2;
+            if(ny<0||nx<0||ny>=n||nx>=m) continue;
+            if(!visited[ny][nx]&&tmp_arr[ny][nx]==0) {
+                visited[ny][nx]=true;
+                cnt++;
+                tmp.push({ny,nx});
             }
         }
     }
+
+    return t_cnt-cnt;
 }
 
-void func(int here,vector<int> &v2) {
+void combi(int here) {
     if(v2.size()==3) {
-        fill(&tmp[0][0],&tmp[0][0]+10*10,0);
-        fill(&visited[0][0],&visited[0][0]+10*10,0);
-
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) tmp[i][j]=arr[i][j];
+        for(int i=0;i<15;i++) {
+            for(int j=0;j<15;j++) tmp_arr[i][j]=arr[i][j];
         }
+        for(auto iter:v2) tmp_arr[v1[iter].first][v1[iter].second]=1;
 
-        for(int i=0;i<v2.size();i++) {
-            int y=v1[v2[i]].first, x=v1[v2[i]].second;
+        mx=max(mx,bfs());
 
-            tmp[y][x]=1;    // 벽 채우기
-        }
-
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(!visited[i][j] && tmp[i][j]==2) {
-                    bfs(i,j);
-                }
-            }
-        }
-        int cnt=0;
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(tmp[i][j]==0) cnt++;
-            }
-        }
-
-        mx=max(mx,cnt);
         return;
     }
-    for(int i=here+1;i<v1.size();i++) {
+    for(int i=here+1;i<(int)v1.size();i++) {
         v2.push_back(i);
-        func(i,v2);
+        combi(i);
         v2.pop_back();
     }
 }
 
 int main() {
-
     scanf("%d %d",&n,&m);
-
     for(int i=0;i<n;i++) {
         for(int j=0;j<m;j++) {
             scanf("%d",&arr[i][j]);
-
-            if(arr[i][j]==0) v1.push_back({i,j});   // 빈칸의 좌표 저장
+            if(arr[i][j]==2) virus.push({i,j});
+            if(arr[i][j]==0) {
+                v1.push_back({i,j});
+                t_cnt++;
+            }
         }
     }
+    combi(-1);
+    printf("%d\n",mx-3);
 
-    func(-1,v2);
-
-    printf("%d\n",mx);
+    return 0;
 }
