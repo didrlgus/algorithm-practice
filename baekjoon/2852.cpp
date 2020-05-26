6e1687900a935d1c1dba52e46546f9d055829478
양기현 <rlgusdid@naver.com>
2020-05-26 22:39:52 +0900

    [baekjoon] NBA 농구

--- baekjoon/2852.cpp
+++ baekjoon/2852.cpp
@@ -1,71 +1,37 @@
 // NBA 농구
 #include<bits/stdc++.h>
-
 using namespace std;
 
-int n;
-int team1[2900];
-int team2[2900];
-vector<int> v1,v2;
+int n,arr1[2890],arr2[2890],cnt1,cnt2;
 
-string func(int a,int b) {
-    string rs;
-    if(a<10) rs+='0';
-    rs+=to_string(a);
-
-    rs+=':';
-
-    if(b<10) rs+='0';
-    rs+=to_string(b);
-
-    return rs;
+string to_minute(int m,int s) {
+    string str="";
+    if(m<10) str+='0';
+    str+=to_string(m);
+    str+=':';
+    if(s<10) str+='0';
+    str+=to_string(s);
+    return str;
 }
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     cin>>n; cin.ignore();
-
     for(int i=0;i<n;i++) {
-        string s;
-        getline(cin,s);
-        
-        int team,minute,second;
-
-        team=atoi(s.substr(0,1).c_str());
-        minute=atoi(s.substr(2,3).c_str());
-        second=atoi(s.substr(5).c_str());
-
-        if(team==1) {
-            v1.push_back(minute*60+second);      // 초로 변환
-        } else {
-            v2.push_back(minute*60+second);
+        string str;
+        getline(cin,str);
+        int t,m,s;
+        t=atoi(str.substr(0,1).c_str());
+        m=atoi(str.substr(2,4).c_str());
+        s=atoi(str.substr(5).c_str());
+        for(int j=m*60+s;j<2880;j++) {
+            if(t==1) arr1[j]++;
+            else arr2[j]++;       
         }
-    }   
-    sort(v1.begin(),v1.end());
-    sort(v2.begin(),v2.end());
-
-    for(int i=0;i<v1.size();i++) {
-        for(int j=v1[i];j<2880;j++) team1[j]=i+1;
     }
-
-    for(int i=0;i<v2.size();i++) {
-        for(int j=v2[i];j<2880;j++) team2[j]=i+1;
-    }
-
-    int lead1=0,lead2=0;
     for(int i=0;i<2880;i++) {
-        if(team1[i]>team2[i]) lead1++;
-        else if(team1[i]<team2[i]) lead2++;
+        if(arr1[i]>arr2[i]) cnt1++;
+        else if(arr1[i]<arr2[i]) cnt2++;
     }
-    
-    int a=(lead1/60), b=(lead1%60);
-    int c=(lead2/60), d=(lead2%60);
-    
-    cout<<func(a,b)<<"\n";
-    cout<<func(c,d)<<"\n";
-    
+    cout<<to_minute(cnt1/60,cnt1%60)<<"\n";
+    cout<<to_minute(cnt2/60,cnt2%60)<<"\n";
     return 0;
 }
