7d58319c54694ddf3ff4e43405a09fa768287bde
양기현 <rlgusdid@naver.com>
2020-05-27 23:57:57 +0900

    [baekjoon] 뮤탈리스크

--- baekjoon/12869.cpp
+++ baekjoon/12869.cpp
@@ -1,49 +1,37 @@
 // 뮤탈리스크
 #include<bits/stdc++.h>
-
 using namespace std;
 
-int n;
-int arr[3];
-int arr2[6][3]={{9,3,1},{9,1,3},{3,9,1},{3,1,9},{1,9,3},{1,3,9}};
-queue<pair<int,pair<int,int>>> q;
-
-int visited[65][65][65];
-
+int n,arr[5],level;
+int permu[6][3]={{9,3,1},{9,1,3},{3,9,1},{3,1,9},{1,9,3},{1,3,9}};
+bool visited[65][65][65];
+struct p {
+    int a,b,c;
+};
 int bfs(int a,int b,int c) {
-    visited[a][b][c]=1;
-    q.push({a,{b,c}});
-    
+    queue<p> q;
+    q.push({a,b,c});
+    visited[a][b][c]=true;
     while(!q.empty()) {
-        a=q.front().first, b=q.front().second.first, c=q.front().second.second;
-        q.pop();
-
-        if(a<=0 && b<=0 && c<=0) return visited[a][b][c]-1;
-
-        for(int i=0;i<6;i++) {
-            int na=max(0,a-arr2[i][0]),nb=max(0,b-arr2[i][1]),nc=max(0,c-arr2[i][2]);
-
-            if(!visited[na][nb][nc]) {
-                visited[na][nb][nc]=visited[a][b][c]+1;
-                q.push({na,{nb,nc}});
-            } 
+        int size=q.size();
+        while(size--) {
+            a=q.front().a,b=q.front().b,c=q.front().c;q.pop();
+            if(a==0&&b==0&&c==0) return level;
+            for(int i=0;i<6;i++) {
+                int d=max(0,a-permu[i][0]),e=max(0,b-permu[i][1]),f=max(0,c-permu[i][2]);
+                if(!visited[d][e][f]) {
+                    visited[d][e][f]=true;
+                    q.push({d,e,f});
+                }
+            }
         }
+        level++;
     }
     return -1;
 }
-
 int main() {
-
     scanf("%d",&n);
-
-    for(int i=0;i<n;i++) {
-        int a;
-        scanf("%d",&a);
-        arr[i]=a;
-    }
-
-    int result=bfs(arr[0],arr[1],arr[2]);
-    printf("%d\n",result);
-
+    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
+    printf("%d\n",bfs(arr[0],arr[1],arr[2]));
     return 0;
 }
