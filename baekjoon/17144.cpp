5f2ac58eb126ea07304788362a23c6c6291b8644
양기현 <rlgusdid@naver.com>
2020-06-02 00:34:52 +0900

    [baekjoon] 미세먼지 안녕!

--- baekjoon/17144.cpp
+++ baekjoon/17144.cpp
@@ -1,133 +1,100 @@
 // 미세먼지 안녕!
 #include<bits/stdc++.h>
-
 using namespace std;
+int n,m,t,arr[55][55],tmp[55][55],ret;
+bool visited[55][55];
+vector<pair<int,int>> v,v2;
+vector<int> v3;
+int dy[]={0,-1,0,1};
+int dx[]={1,0,-1,0};
 
-int n,m,t;
-int arr[55][55];
-
-int dy[]={0,0,-1,1};
-int dx[]={-1,1,0,0};
-
-int upY,upX,downY,downX;
-
-void bfs() {
-    queue<pair<int,int>> q;
-    for(int i=1;i<=n;i++) {
-        for(int j=1;j<=m;j++) {
-            if(arr[i][j]>0) {
-                q.push({i,j});
-            }
-        }
+void dfs(int y,int x,int dir) {
+    if(arr[y][x]==-1) return;
+    if(x==m) {
+        dfs(y-1,x-1,dir+1);
+        return;
     }
-    
-    int tmp[55][55]={0,};
-    for(int i=1;i<=n;i++) {
-        for(int j=1;j<=m;j++) tmp[i][j]=arr[i][j];
+    if(y==-1) {
+        dfs(y+1,x-1,dir+1);
+        return;
     }
-
-    while(q.size()) {
-        int y=q.front().first, x=q.front().second;
-        q.pop();
-
-        int cnt=0;
-        for(int i=0;i<4;i++) {
-            int ny=y+dy[i],nx=x+dx[i];
-
-            if(ny<1||nx<1||ny>=n+1||nx>=m+1) continue;
-            if(arr[ny][nx]==-1) continue;
-            cnt++;
-            arr[ny][nx]+=(tmp[y][x]/5);
-            
-        }   
-        arr[y][x]-=(tmp[y][x]/5)*cnt;
+    if(x==-1) {
+        dfs(y+1,x+1,dir+1);
+        return;
     }
+    v2.push_back({y,x});
+    v3.push_back({arr[y][x]});
+    dfs(y+dy[dir],x+dx[dir],dir);
+    return;
 }
-
+void dfs2(int y,int x,int dir) {
+    if(arr[y][x]==-1) return;
+    if(x==m) {
+        if(dir-1==-1) dir=4;
+        dfs2(y+1,x-1,dir-1);
+        return;
+    }
+    if(y==n) {
+        dfs2(y-1,x-1,dir-1);
+        return;
+    }
+    if(x==-1) {
+        dfs2(y-1,x+1,dir-1);
+        return;
+    }
+    v2.push_back({y,x});
+    v3.push_back({arr[y][x]});
+    dfs2(y+dy[dir],x+dx[dir],dir);
+    return;
+}
 int main() {
-
     scanf("%d %d %d",&n,&m,&t);
-    
-    int flag=false;
-    for(int i=1;i<=n;i++) {
-        for(int j=1;j<=m;j++) {
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
             scanf("%d",&arr[i][j]);
-            
-            if(!flag && arr[i][j]==-1) {
-                upY=i; upX=j; flag=true;
-            } else if(flag && arr[i][j]==-1){
-                downY=i; downX=j;
+            if(arr[i][j]==-1) v.push_back({i,j});
+        }
+    }
+    while(t--) {
+        fill(&tmp[0][0],&tmp[0][0]+55*55,0);
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(arr[i][j]>0) {
+                    int cnt=0;
+                    for(int k=0;k<4;k++) {
+                        int ny=i+dy[k],nx=j+dx[k];
+                        if(ny<0||nx<0||ny>=n||nx>=m||arr[ny][nx]==-1) continue;
+                        tmp[ny][nx]+=arr[i][j]/5;
+                        cnt++;
+                    }
+                    tmp[i][j]+=arr[i][j]-arr[i][j]/5*cnt;
+                }
             }
         }
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(tmp[i][j]>0) {
+                    arr[i][j]=tmp[i][j];
+                }
+            }
+        }
+        int y=v[0].first,x=v[0].second;
+        dfs(y,x+1,0);
+        rotate(v3.begin(),v3.begin()+v3.size()-1,v3.end());
+        v3[0]=0;
+        for(int i=0;i<(int)v2.size();i++) arr[v2[i].first][v2[i].second]=v3[i];
+        v2.clear();v3.clear();
+        y=v[1].first,x=v[1].second;
+        dfs2(y,x+1,0);
+        rotate(v3.begin(),v3.begin()+v3.size()-1,v3.end());
+        v3[0]=0;
+        for(int i=0;i<(int)v2.size();i++) arr[v2[i].first][v2[i].second]=v3[i];
+        v2.clear();v3.clear();
+    }    
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) if(arr[i][j]>0) ret+=arr[i][j];
     }
-    
-    while(t--) {
-        bfs();
-        // 위쪽 청정기
-        // 오른쪽
-        for(int i=m;i>=2;i--) {
-            arr[upY][i+1]=arr[upY][i];
-        }
-        arr[upY][2]=0;
-
-        // 위
-        for(int i=1;i<upY;i++) {
-            arr[i-1][m]=arr[i][m];
-        }
-        arr[upY-1][m]=arr[upY][m+1];
-        arr[upY][m+1]=0;
-
-        // 왼쪽
-        for(int i=1;i<=m-1;i++) {
-            arr[1][i-1]=arr[1][i];
-        }
-        arr[1][m-1]=arr[0][m];
-        arr[0][m]=0;
-
-        // 아래
-        for(int i=upY-2;i>=2;i--) {
-            arr[i+1][1]=arr[i][1];
-        }
-        arr[2][1]=arr[1][0];
-        arr[1][0]=0;
-
-        // 아래쪽 청정기
-        // 오른쪽
-        for(int i=m;i>=2;i--) {
-            arr[downY][i+1]=arr[downY][i];
-        }
-        arr[downY][2]=0;
-
-        // 아래쪽
-        for(int i=n;i>downY;i--) {
-            arr[i+1][m]=arr[i][m];
-        }
-        arr[downY+1][m]=arr[downY][m+1];
-        arr[downY][m+1]=0;
-
-        // 왼쪽
-        for(int i=1;i<=m;i++) {
-            arr[n][i-1]=arr[n][i];
-        }
-        arr[n][m-1]=arr[n+1][m];
-        arr[n+1][m]=0;
-
-        // 위
-        for(int i=downY+2;i<=n-1;i++) {
-            arr[i-1][1]=arr[i][1];
-        }
-        arr[n-1][1]=arr[n][0];
-        arr[n][0]=0;
-    }
-
-    int sum=0;
-    for(int i=1;i<=n;i++) {
-        for(int j=1;j<=m;j++) {
-            if(arr[i][j]>0) sum+=arr[i][j];
-        }
-    }
-
-    printf("%d\n",sum);
+    printf("%d\n",ret);
 
     return 0;
 }
