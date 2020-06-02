3d17643c0c053c4d80dd771f723734ab6aeda1ee
양기현 <rlgusdid@naver.com>
2020-06-02 19:29:43 +0900

    [baekjoon] 평범한 배낭

--- baekjoon/12865.cpp
+++ baekjoon/12865.cpp
@@ -1,26 +1,13 @@
 // 평범한 배낭
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n,k,w[110],v[110];
-vector<int> dp1;
-vector<int> dp2;
+int n,k,w[110],v[110],dp[100010]; 
 int main() {
     scanf("%d %d",&n,&k);
-    dp1.resize(k+1);
-    dp2.resize(k+1);
-    for(int i=1;i<=n;i++) {
-        scanf("%d %d",&w[i],&v[i]);
+    for(int i=0;i<n;i++) scanf("%d %d",&w[i],&v[i]);
+    for(int i=0;i<n;i++) {
+        for(int j=k;j>=w[i];j--) dp[j]=max(dp[j],dp[j-w[i]]+v[i]);
     }
-    // dp[i][j]=max(dp[i-1][j],dp[i-1][j-w[i]]+v[i])
-    for(int i=1;i<=n;i++) {
-        for(int j=1;j<=k;j++) {
-            if(j>=w[i]) dp2[j]=max(dp1[j],dp1[j-w[i]]+v[i]);
-            else dp2[j]=dp1[j];
-        }
-        dp1=dp2;
-    }
-    printf("%d\n",dp1[k]);
+    printf("%d\n",dp[k]);
     return 0;
 }
