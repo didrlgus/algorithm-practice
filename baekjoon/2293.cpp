8ab9e50f1673205af7adbff3c0b80c3453959fb8
양기현 <rlgusdid@naver.com>
2020-06-02 19:41:39 +0900

    [baekjoon] 동전 1

--- baekjoon/2293.cpp
+++ baekjoon/2293.cpp
@@ -1,31 +1,16 @@
 // 동전 1
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-// dp[i][j]->i번째 수를 선택하여 j를 만들 수 있는 모든 경우의 수
-// dp[i][j]=dp[i-1][j]+dp[i][j-arr[i]]
-vector<int> dp1;     // 벡터가 값을 복사하기 쉬우므로 벡터로 선언
-vector<int> dp2;
-int arr[110];
-int n,k;
+int n,k,coin[110],dp[10010];
 int main() {
     scanf("%d %d",&n,&k);
-    dp1.resize(k+1); dp2.resize(k+1);
-    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
-
-    for(int j=0;j<=k;j+=arr[0]) dp1[j]=1;    // base
-    
-    for(int i=1;i<n;i++) {
-        fill(dp2.begin(),dp2.end(),0);
-        for(int j=0;j<=k;j++) {
-            if(arr[i]<=j) dp2[j]=dp1[j]+dp2[j-arr[i]];
-            else dp2[j]=dp1[j];
+    for(int i=0;i<n;i++) scanf("%d",&coin[i]);
+    dp[0]=1;
+    for(int i=0;i<n;i++) {
+        for(int j=coin[i];j<=k;j++) {
+            dp[j]+=dp[j-coin[i]];
         }
-        dp1=dp2;
     }
-
-    printf("%d\n",dp1[k]);
-
+    printf("%d\n",dp[k]);
     return 0;
 }
