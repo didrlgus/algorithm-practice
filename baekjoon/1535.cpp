b07f07ca00608ec1e4739561659416d1ccc6967b
양기현 <rlgusdid@naver.com>
2020-06-02 19:02:23 +0900

    [baekjoon] 안녕

--- baekjoon/1535.cpp
+++ baekjoon/1535.cpp
@@ -1,31 +1,14 @@
 // 안녕
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n,L[110],J[110];
-vector<int> dp1,dp2;
+int n,L[110],J[110],dp[110];
 int main() {
-
     scanf("%d",&n);
-    dp1.resize(100);
-    dp2.resize(100);
-    for(int i=1;i<=n;i++) {
-        scanf("%d",&L[i]);
+    for(int i=0;i<n;i++) scanf("%d",&L[i]);
+    for(int i=0;i<n;i++) scanf("%d",&J[i]);
+    for(int i=0;i<n;i++) {
+        for(int j=99;j>=L[i];j--) dp[j]=max(dp[j],dp[j-L[i]]+J[i]);
     }
-    for(int i=1;i<=n;i++) {
-        scanf("%d",&J[i]);
-    }
-
-    for(int i=1;i<=n;i++) {
-        fill(dp2.begin(),dp2.end(),0);
-        for(int j=1;j<=99;j++) {
-            if(j>=L[i]) dp2[j]=max(dp1[j],dp1[j-L[i]]+J[i]);
-            else dp2[j]=dp1[j];
-        }
-        dp1=dp2;
-    }
-    printf("%d\n",dp1[99]);
-
+    printf("%d\n",dp[99]);
     return 0;
 }
