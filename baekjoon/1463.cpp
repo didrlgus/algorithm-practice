77f53bfc00510d6fd8a59245db06d252cc31e7c5
양기현 <rlgusdid@naver.com>
2020-06-02 22:07:17 +0900

    [baekjoon] 1로 만들기

--- baekjoon/1463.cpp
+++ baekjoon/1463.cpp
@@ -1,27 +1,20 @@
 // 1로 만들기
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n;
-int dp[1000010];
+const int INF=987654321;
+int n,dp[1000010];
 int dfs(int x) {
     if(x==1) return 0;
-    if(dp[x]!=-1) return dp[x];
-
     int& ret=dp[x];
-    int one=dfs(x-1)+1;
-    if(x%2==0) one=min(one,dfs(x/2)+1);
-    if(x%3==0) one=min(one,dfs(x/3)+1);
-    ret=one;
+    if(ret!=-1) return dp[x];
+    ret=INF;
+    ret=dfs(x-1)+1;
+    if(x%3==0) ret=min(ret,dfs(x/3)+1);
+    if(x%2==0) ret=min(ret,dfs(x/2)+1);
     return ret;
 }
-
 int main() {
     scanf("%d",&n);
-
-    fill(&dp[0],&dp[0]+1000010,-1);
+    fill(dp,dp+1000010,-1);
     printf("%d\n",dfs(n));
-
-    return 0;
 }
