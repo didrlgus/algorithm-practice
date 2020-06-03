53cae069a2d190abd656fbf919c6a76c16cfe080
양기현 <rlgusdid@naver.com>
2020-06-03 12:31:17 +0900

    [baekjoon] 1로 만들기 2

--- baekjoon/12852.cpp
+++ baekjoon/12852.cpp
@@ -1,51 +1,26 @@
 // 1로 만들기 2
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n;
-int dp[1000010];
-int arr[1000010];
-vector<int> v;
-int dfs(int x) {
-    if(x==1) return 0;
-    if(dp[x]!=-1) return dp[x];
-    int tmp=1000010;
-    int &ret=dp[x];
-    int one=dfs(x-1)+1;
-    tmp=x-1;
-    if(x%2==0) {
-        int two=dfs(x/2)+1;
-        if(one>two) {
-            one=two;
-            tmp=x/2;
-        }
-    }
-    if(x%3==0) {
-        int three=dfs(x/3)+1;
-        if(one>three) {
-            one=three;
-            tmp=x/3;
-        }
-    }
-
-    arr[x]=tmp;
-    ret=one;
-    return ret;
+const int INF=987654321;
+int n,dp[1000010];
+void dfs(int x) {
+    if(x==0) return;
+    printf("%d ",x);
+    if(x%3==0&&dp[x]==dp[x/3]+1) dfs(x/3);
+    else if(x%2==0&&dp[x]==dp[x/2]+1) dfs(x/2);
+    else if(x-1>=0&&dp[x]==dp[x-1]+1) dfs(x-1);
+    return;
 }
 int main() {
     scanf("%d",&n);
-    fill(&dp[0],&dp[0]+1000010,-1);
-    if(n==1) {
-        printf("0\n1\n");
-        return 0;
+    fill(dp,dp+1000010,INF);
+    dp[1]=0;
+    for(int i=2;i<=n;i++) {
+        if(i%3==0) dp[i]=min(dp[i],dp[i/3]+1);
+        if(i%2==0) dp[i]=min(dp[i],dp[i/2]+1);
+        dp[i]=min(dp[i],dp[i-1]+1);
     }
-    printf("%d\n",dfs(n));
-    printf("%d ",n);
-    while (arr[n]!=1) {
-        printf("%d ",arr[n]);
-        n=arr[n];
-    }
-    printf("1\n");
+    printf("%d\n",dp[n]);
+    dfs(n);
     return 0;
 }
