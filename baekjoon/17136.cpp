623aa7b63984c5ecee54d296a9d6c8c547673d1a
양기현 <rlgusdid@naver.com>
2020-06-04 19:22:38 +0900

    [baekjoon] 색종이 붙이기

--- baekjoon/17136.cpp
+++ baekjoon/17136.cpp
@@ -1,30 +1,28 @@
 // 색종이 붙이기
 #include<bits/stdc++.h>
 using namespace std;
-
 const int INF=987654321;
-int arr[15][15],ret=INF,n=10;
-int cnt_arr[10];
+int arr[15][15],cnt_arr[10],n=10,ret=INF;
 bool check(int y,int x,int size) {
-    if(y+size>n||x+size>n) return false;
+    if(y+size>10||x+size>10) return false;
     for(int i=y;i<y+size;i++) {
         for(int j=x;j<x+size;j++) if(!arr[i][j]) return false;
     }
     return true;
 }
-void draw(int y,int x,int size,int val) {
+void draw(int y,int x,int size,bool val) {
     for(int i=y;i<y+size;i++) {
         for(int j=x;j<x+size;j++) arr[i][j]=val;
     }
 }
 void dfs(int y,int x,int cnt) {
     if(cnt>=ret) return;
-    if(x==n) {
-        dfs(y+1,0,cnt);
+    if(y>=n) {
+        ret=min(ret,cnt);
         return;
     }
-    if(y==n) {
-        ret=min(ret,cnt);
+    if(x>=n) {
+        dfs(y+1,0,cnt);
         return;
     }
     if(arr[y][x]==0) {
@@ -33,24 +31,20 @@ void dfs(int y,int x,int cnt) {
     }
     for(int i=5;i>=1;i--) {
         if(cnt_arr[i]==5) continue;
-        if(check(y,x,i)) {
-            cnt_arr[i]++;
-            draw(y,x,i,0);
-            dfs(y,x+i,cnt+1);
-            cnt_arr[i]--;
-            draw(y,x,i,1);
-        }
+        if(!check(y,x,i)) continue;
+        cnt_arr[i]++;
+        draw(y,x,i,0);
+        dfs(y,x+i,cnt+1);
+        draw(y,x,i,1);
+        cnt_arr[i]--;
     }
     return;
 }
-
 int main() {
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<n;j++) scanf("%d",&arr[i][j]);
+    for(int i=0;i<10;i++) {
+        for(int j=0;j<10;j++) scanf("%d",&arr[i][j]);
     }
-
     dfs(0,0,0);
     if(ret==INF) printf("-1\n");
     else printf("%d\n",ret);
-    return 0;
 }
