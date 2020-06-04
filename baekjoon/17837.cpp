d4c546cdc697b55e7db1107e23d5e8ee205e1fc0
양기현 <rlgusdid@naver.com>
2020-06-04 17:05:03 +0900

    [baekjoon] 새로운 게임 2

--- baekjoon/17837.cpp
+++ baekjoon/17837.cpp
@@ -1,95 +1,60 @@
 // 새로운 게임 2
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n,k;
-int arr[15][15];
+int n,k,arr[15][15],ret;
 struct p {
     int y,x,dir;
 };
-vector<p> chess;
-vector<int> status[15][15];
-
+vector<int> horse[15][15];
+vector<p> v;
 int dy[]={0,0,-1,1};
 int dx[]={1,-1,0,0};
-
-bool isExit() {
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<n;j++) {
-            if((int)status[i][j].size()>=4) return true;
-        }    
-    }
-    return false;
-}
-
 int change(int dir) {
     if(dir==0) return 1;
     else if(dir==1) return 0;
     else if(dir==2) return 3;
-    else return 2;
+    else return 2;    
 }
-
 void go(int y,int x,int dir,int idx) {
     int ny=y+dy[dir],nx=x+dx[dir];
-
     if(ny<0||nx<0||ny>=n||nx>=n||arr[ny][nx]==2) {
         dir=change(dir);
-        chess[idx].dir=dir;
-        ny=y+dy[dir],nx=x+dx[dir];
-        if(ny<0||nx<0||ny>=n||nx>=n||arr[ny][nx]==2) return;    // 또 파란색->방향만 바꿔주고 제자리
+        v[idx].dir=dir;
+        ny=y+dy[dir];nx=x+dx[dir];
+        if(ny<0||nx<0||ny>=n||nx>=n||arr[ny][nx]==2) return;
     }
-    
-    // 현재 위치 기준으로 뒤쪽말에 해당하는 부분 찾아서 처리하기
-    vector<int> &tmp=status[y][x];
-    auto pos=find(tmp.begin(),tmp.end(),idx);
-    
+    vector<int>& tmp=horse[y][x];
+    auto it=find(tmp.begin(),tmp.end(),idx);
     vector<int> bak;
-    for(auto it=pos;it!=tmp.end();it++){
-        chess[*it].y=ny;                                  // 체스말도 다 새로 옮겨줘야 함
-        chess[*it].x=nx;
-        bak.push_back(*it);
+    for(auto i=it;i<tmp.end();i++) {
+        v[*i].y=ny;
+        v[*i].x=nx;
+        bak.push_back(*i);
     }
-    tmp.erase(pos,tmp.end());
-
-    if(arr[ny][nx]==1) reverse(bak.begin(),bak.end());      // 빨간색->reverse
-    
-    for(int el:bak) status[ny][nx].push_back(el);
+    tmp.erase(it,tmp.end());
+    if(arr[ny][nx]==1) reverse(bak.begin(),bak.end());
+    for(auto it:bak) horse[ny][nx].push_back(it); 
+    if(horse[ny][nx].size()>=4) {
+        printf("%d\n",ret);
+        exit(0);
+    }
 }
-
 int main() {
     scanf("%d %d",&n,&k);
     for(int i=0;i<n;i++) {
-        for(int j=0;j<n;j++) {
-            scanf("%d",&arr[i][j]);
-        }
+        for(int j=0;j<n;j++) scanf("%d",&arr[i][j]);
     }
     for(int i=0;i<k;i++) {
         int a,b,c;
         scanf("%d %d %d",&a,&b,&c);
         a--;b--;c--;
-        chess.push_back({a,b,c});
-        status[a][b].push_back(i);
+        horse[a][b].push_back(i);
+        v.push_back({a,b,c});
     }
-    
-    int turn=0;
-    bool flag=false;
-    while(turn<=1000) {
-        turn++;
-        for(int i=0;i<(int)chess.size();i++) {
-            int y=chess[i].y,x=chess[i].x,dir=chess[i].dir;
-            go(y,x,dir,i);
-            
-            if(isExit()) {                  // 체스말을 하나하나 옮기면서 4개이상 쌓이면 '바로' 게임 종료
-                flag=true;
-                break;
-            }
-        }
-        if(flag) break;
+    while(ret<=1000) {
+        ret++;
+        for(int i=0;i<k;i++) go(v[i].y,v[i].x,v[i].dir,i);
     }
-
-    if(flag) printf("%d\n",turn);
-    else printf("-1\n");
-
+    printf("-1\n");
     return 0;
 }
