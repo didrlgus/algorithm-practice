b5a9f10c5714ed96ef2170a3c525989fec88b230
양기현 <rlgusdid@naver.com>
2020-06-06 14:46:42 +0900

    [baekjoon] 극장 좌석

--- baekjoon/2302.cpp
+++ baekjoon/2302.cpp
@@ -1,28 +1,22 @@
 // 극장 좌석
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int n,m,ret=1;
-ll dp[45];
+int n,m,dp[45],ret=1;
 vector<int> v;
 int main() {
-    scanf("%d %d",&n,&m);
-    for(int i=0;i<m;i++) {
-        int a;
-        scanf("%d",&a);
-        v.push_back(a);
-    }
-    dp[0]=1;dp[1]=1;
-    for(int i=2;i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
-    if((int)v.size()==0) {
-        printf("%d\n",dp[n]);
-        return 0;
-    }
-    ret*=dp[v[0]-1];
-    for(int i=1;i<(int)v.size();i++) {
-        ret*=dp[v[i]-v[i-1]-1];
-    }
-    ret*=dp[n-v.back()];
-    printf("%d\n",ret);
-    return 0;
+	scanf("%d %d",&n,&m);
+	dp[0]=1;dp[1]=1;
+	for(int i=2;i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
+	v.push_back(0);
+	for(int i=0;i<m;i++) {
+		int a;
+		scanf("%d",&a);
+		v.push_back(a);
+	}
+	for(int i=m;i>=1;i--) {
+		ret*=dp[v[i]-v[i-1]-1];
+	}
+	ret*=dp[n-v.back()];
+	printf("%d\n",ret);
+	return 0;
 }
