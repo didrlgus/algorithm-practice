5eb26c2cca1744d4893eeb01c65d4fc25f5ad851
양기현 <rlgusdid@naver.com>
2020-06-09 19:41:00 +0900

    [baekjoon] 회의실 배정

--- baekjoon/1931.cpp
+++ baekjoon/1931.cpp
@@ -1,32 +1,23 @@
 // 회의실 배정
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n;
+int n,ret;
 vector<pair<int,int>> v;
-bool cmp(pair<int,int> p1,pair<int,int> p2) {
-    if(p1.second==p2.second) return p1.first<p2.first;
-    return p1.second<p2.second;
-}
-
 int main() {
-
-    scanf("%d",&n);
-    for(int i=0;i<n;i++) {
-        int a,b;
-        scanf("%d %d",&a,&b);
-        v.push_back({a,b});
-    }
-    sort(v.begin(),v.end(),cmp);
-    int e=v[0].second;
-    int cnt=1;
-    for(int i=1;i<n;i++) {
-        if(e<=v[i].first) {
-            e=v[i].second;
-            cnt++;
-        }
-    }
-    printf("%d\n",cnt);
-    return 0;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++) {
+		int s,e;		
+		scanf("%d %d",&s,&e);
+		v.push_back({e,s});
+	}
+	sort(v.begin(),v.end());
+	int e=v[0].first;
+	ret++;
+	for(int i=1;i<n;i++) {
+		if(e>v[i].second) continue;
+		e=v[i].first;
+		ret++;
+	}
+	printf("%d\n",ret);
+	return 0;
 }
