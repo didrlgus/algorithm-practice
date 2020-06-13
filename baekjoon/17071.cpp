5e2e49834566eb77524ce8a17838c4c7c0d98d1a
양기현 <rlgusdid@naver.com>
2020-06-13 22:27:29 +0900

    [baekjoon] 숨바꼭질 5

--- baekjoon/17071.cpp
+++ baekjoon/17071.cpp
@@ -1,55 +1,39 @@
 // 숨바꼭질 5
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int a,b;
+int n,k,dm[]={1,1,2},dx[]={-1,1,0};
+bool visited[3][500010];
 queue<int> q;
-
-int dx[]={-1,1,0};      
-int dm[]={1,1,2};           // -1,+1,*2
-
-bool visited[2][500010];    // 홀짝,범위
-
-int bfs(int n) {
-    visited[0][n]=true;
-    q.push(n);
-    int level=1;
-    while(!q.empty()) {
-        b+=level;
-        if(b>500000) break;
-        if(visited[level%2][b]) return level;           // 이전 단계에 이미 b가 visited 됐을 경우
-        int size=q.size();
-        while(size--) {
-            n=q.front(); q.pop();
-
-            for(int i=0;i<3;i++) {
-                int na=(n+dx[i])*dm[i];
-
-                if(na<0 || na>500000) continue;
-                if(na==b) return level;                 // 처음 b가 나와서 일치하는 경우
-
-                if(!visited[level%2][na]) {
-                    visited[level%2][na]=true;
-                    q.push(na);
-                }
-            }
-        }
-        level++;
-    }
-    return -1;
+int bfs(int x) {
+	int time=0;
+	visited[time][x]=true;
+	q.push(x);
+	while(!q.empty()) {
+		int size=(int)q.size();
+		time++;
+		while(size--) {
+			x=q.front();q.pop();
+			for(int i=0;i<3;i++) {
+				int nx=x*dm[i]+dx[i];
+				if(nx<0||nx>500000) continue;
+				if(!visited[time%2][nx]) {
+					visited[time%2][nx]=true;
+					q.push(nx);
+				}
+			}
+		}
+		k+=time;
+		if(k>500000) break;
+		if(visited[time%2][k]) return time;
+	}
+	return -1;
 }
-
 int main() {
-
-    scanf("%d %d",&a,&b);
-
-    if(a==b) {
-        printf("0\n");
-        return 0;
-    }
-    int result=bfs(a);
-    printf("%d\n",result);
-
-    return 0;
+	scanf("%d %d",&n,&k);
+	if(n==k) {
+		printf("0\n");
+		return 0;
+	}
+	printf("%d\n",bfs(n));
+	return 0;
 }
