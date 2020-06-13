03a4384eba152c112399a9dff06867b34d2ad04f
양기현 <rlgusdid@naver.com>
2020-06-13 21:25:28 +0900

    [baekjoon] 팰린드롬 만들기

--- baekjoon/1213.cpp
+++ baekjoon/1213.cpp
@@ -1,40 +1,28 @@
 // 팰린드롬 만들기
 #include<bits/stdc++.h>
-
 using namespace std;
-
-string s;
-int arr[110],cnt;
-char mid;
+string s,ret;
+int cnt[100];
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin>>s;
-
-    for(char c:s) arr[c]++;
-
-    string ret="";
-    for(int i='Z';i>='A';i--) {
-        if(arr[i]) {            // 알파벳이 존재할 경우
-            if(arr[i]&1) {      // 홀수일 경우
-                cnt++;
-                mid=char(i);
-                arr[i]--;
-            }
-            if(cnt>=2) break;
-            for(int j=0;j<arr[i];j+=2) {
-                ret=char(i)+ret;
-                ret+=char(i);
-            }
-        }
-    }
-    // 홀수개의 알파벳이 있는경우
-    if(mid) ret.insert(ret.begin()+ret.size()/2,mid);
-    
-    if(cnt>=2) cout<<"I'm Sorry Hansoo\n";
-    else cout<<ret<<"\n";
-
-    return 0;
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	cin>>s;
+	for(auto c:s) cnt[c]++;
+	bool flag=false;
+	for(int i='Z';i>='A';i--) {
+		if(cnt[i]&1) {
+			if(flag) {
+				cout<<"I'm Sorry Hansoo"<<'\n';
+				return 0;
+			}
+			cnt[i]--;
+			ret.insert(ret.begin()+(int)ret.size()/2,char(i));
+			flag=true;
+		}
+		for(int j=0;j<cnt[i];j+=2) {
+			ret=char(i)+ret;
+			ret+=char(i);
+		}	
+	}
+	cout<<ret<<'\n';
+	return 0;
 }
