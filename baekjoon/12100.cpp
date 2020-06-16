882013a50467f0a489095fe27a7669112b9d4f8f
양기현 <rlgusdid@naver.com>
2020-06-17 00:58:33 +0900

    [baekjoon] 2048 (Easy)

--- baekjoon/12100.cpp
+++ baekjoon/12100.cpp
@@ -1,134 +1,123 @@
 // 2048 (Easy)
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n;
-int arr[25][25];
-int tmp[25][25];
-int mx=-987654321;
+const int INF=-987654321;
+int n,arr[25][25],tmp[25][25],ret=INF;
 vector<int> v;
-
 void go(int type) {
-    queue<int> q;
-    if(type==0) {                               // 오른쪽
-        for(int i=0;i<n;i++) {
-            for(int j=n-1;j>=0;j--) {
-                if(tmp[i][j]>0) {
-                    q.push(tmp[i][j]);
-                    tmp[i][j]=0;
-                }
-            }
-            int idx=n-1;
-            while(!q.empty()) {
-                int f=q.front(); q.pop();
-                if(tmp[i][idx]==0) {
-                    mx=max(mx,f);
-                    tmp[i][idx]=f;
-                } else if(tmp[i][idx]==f) {
-                    mx=max(mx,f*2);
-                    tmp[i][idx--]*=2;
-                } else {
-                    mx=max(mx,f);
-                    tmp[i][--idx]=f;
-                }
-            }
-        }
-    } else if(type==1) {                        // 왼쪽
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                if(tmp[i][j]>0) {
-                    q.push(tmp[i][j]);
-                    tmp[i][j]=0;
-                }
-            }
-            int idx=0;
-            while(!q.empty()) {
-                int f=q.front(); q.pop();
-                if(tmp[i][idx]==0) {
-                    mx=max(mx,f);
-                    tmp[i][idx]=f;
-                } else if(tmp[i][idx]==f) {
-                    mx=max(mx,f*2);
-                    tmp[i][idx++]*=2;
-                } else {
-                    mx=max(mx,f);
-                    tmp[i][++idx]=f;
-                }
-            }
-        }
-    } else if(type==2) {                        // 위쪽
-        for(int j=0;j<n;j++) {
-            for(int i=0;i<n;i++) {
-                if(tmp[i][j]>0) {
-                    q.push(tmp[i][j]);
-                    tmp[i][j]=0;
-                }
-            }
-            int idx=0;
-            while(!q.empty()) {
-                int f=q.front(); q.pop();
-                if(tmp[idx][j]==0) {
-                    mx=max(mx,f);
-                    tmp[idx][j]=f;
-                } else if(tmp[idx][j]==f) {
-                    mx=max(mx,f*2);
-                    tmp[idx++][j]*=2;
-                } else {
-                    mx=max(mx,f);
-                    tmp[++idx][j]=f;
-                }
-            }
-        }
-    } else {
-        for(int j=0;j<n;j++) {
-            for(int i=n-1;i>=0;i--) {
-                if(tmp[i][j]>0) {
-                    q.push(tmp[i][j]);
-                    tmp[i][j]=0;
-                }
-            }
-            int idx=n-1;
-            while(!q.empty()) {
-                int f=q.front(); q.pop();
-                if(tmp[idx][j]==0) {
-                    mx=max(mx,f);
-                    tmp[idx][j]=f;
-                } else if(tmp[idx][j]==f) {
-                    mx=max(mx,f*2);
-                    tmp[idx--][j]*=2;
-                } else {
-                    mx=max(mx,f);
-                    tmp[--idx][j]=f;
-                }
-            }
-        }
-    }
+	queue<int> q;
+	if(type==0) {
+		for(int j=0;j<n;j++) {
+			for(int i=0;i<n;i++) {
+				if(tmp[i][j]!=0) {
+					q.push(tmp[i][j]);
+					tmp[i][j]=0;
+				}
+			}
+			int idx=0;
+			while(!q.empty()) {
+				int f=q.front();q.pop();
+				if(tmp[idx][j]==0) {
+					tmp[idx][j]=f;
+					ret=max(ret,f);
+				} else if(tmp[idx][j]==f) {
+					tmp[idx++][j]=2*f;
+					ret=max(ret,2*f);
+				} else {
+					tmp[++idx][j]=f;
+					ret=max(ret,f);
+				}
+			}
+		}
+	} else if(type==1) {
+		for(int j=0;j<n;j++) {
+			for(int i=n-1;i>=0;i--) {
+				if(tmp[i][j]!=0) {
+					q.push(tmp[i][j]);
+					tmp[i][j]=0;
+				}
+			}
+			int idx=n-1;
+			while(!q.empty()) {
+				int f=q.front();q.pop();
+				if(tmp[idx][j]==0) {
+					tmp[idx][j]=f;
+					ret=max(ret,f);
+				} else if(tmp[idx][j]==f) {
+					tmp[idx--][j]=2*f;
+					ret=max(ret,2*f);
+				} else {
+					tmp[--idx][j]=f;
+					ret=max(ret,f);
+				}
+			}
+		}
+	} else if(type==2) {
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(tmp[i][j]!=0) {
+					q.push(tmp[i][j]);
+					tmp[i][j]=0;
+				}
+			}
+			int idx=0;
+			while(!q.empty()) {
+				int f=q.front();q.pop();
+				if(tmp[i][idx]==0) {
+					tmp[i][idx]=f;
+					ret=max(ret,f);
+				} else if(tmp[i][idx]==f) {
+					tmp[i][idx++]=2*f;
+					ret=max(ret,2*f);
+				} else {
+					tmp[i][++idx]=f;
+					ret=max(ret,f);
+				}
+			}
+		}
+	} else {
+		for(int i=0;i<n;i++) {
+			for(int j=n-1;j>=0;j--) {
+				if(tmp[i][j]!=0) {
+					q.push(tmp[i][j]);
+					tmp[i][j]=0;
+				}
+			}
+			int idx=n-1;
+			while(!q.empty()) {
+				int f=q.front();q.pop();
+				if(tmp[i][idx]==0) {
+					tmp[i][idx]=f;
+					ret=max(ret,f);
+				} else if(tmp[i][idx]==f) {
+					tmp[i][idx--]=2*f;
+					ret=max(ret,2*f);
+				} else {
+					tmp[i][--idx]=f;
+					ret=max(ret,f);
+				}
+			}
+		}
+	}
 }
-
-void permu() {
-    if(v.size()>=5) {
-        memcpy(tmp,arr,sizeof(tmp));
-        for(int i=0;i<v.size();i++) go(v[i]);
-        
-        return;
-    }
-    for(int i=0;i<4;i++) {      // 0->오른쪽, 1->왼쪽, 2->위쪽, 3->아래쪽
-        v.push_back(i);
-        permu();
-        v.pop_back();
-    }
+void permu(vector<int>& v) {
+	if((int)v.size()==5) {
+		memcpy(tmp,arr,sizeof(arr));
+		for(auto iter:v) go(iter);
+		return;
+	}
+	for(int i=0;i<4;i++) {
+		v.push_back(i);
+		permu(v);
+		v.pop_back();
+	}
 }
-
 int main() {
-    scanf("%d",&n);
-
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<n;j++) scanf("%d",&arr[i][j]);
-    }
-
-    permu();
-    printf("%d\n",mx);
-
-    return 0;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<n;j++) scanf("%d",&arr[i][j]);
+	}
+	permu(v);
+	printf("%d\n",ret);
+	return 0;
 }
