7cefdad49708a6b6a701fef20b04f253e3cf1e15
양기현 <rlgusdid@naver.com>
2020-06-17 13:36:30 +0900

    [baekjoon] 수들의 합2

--- baekjoon/2003.cpp
+++ baekjoon/2003.cpp
@@ -1,46 +1,17 @@
 // 2003. 수들의 합2
-#include<cstdio>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int n,m;
-int a[10010];
-
-int high=0,low=0;
-int cnt=0;
-
-// 투 포인터 알고리즘
-// (배열의 연속합 구하는데 사용)
-void func() {
-    int sum=a[0];
-
-    while(high<n) {
-        if(sum<m) {
-            sum+=a[++high];
-        } else if(sum==m) {
-            cnt++;
-            sum+=a[++high];
-        } else if(sum>m) {
-            sum-=a[low++];
-
-            if(low>high && low<n) {
-                high=low;
-                sum=a[high];
-            }
-        }
-    }
-}
-
+int n,m,l,h,sum,ret;
 int main() {
-
     scanf("%d %d",&n,&m);
-
-    for(int i=0;i<n;i++) scanf("%d",&a[i]);
-    
-    func();
-
-    printf("%d\n",cnt);
-
-
+    vector<int> v(n);
+    for(int i=0;i<n;i++) scanf("%d",&v[i]);
+    while(true) {
+        if(sum>=m) sum-=v[l++];
+        else if(h==n) break;
+        else sum+=v[h++];
+        if(sum==m) ret++;
+    }
+    printf("%d\n",ret);
     return 0;
 }
