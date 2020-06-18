26f63b6e8edf2d9d1d8e4d89bef0dad82e03648b
양기현 <rlgusdid@naver.com>
2020-06-18 17:27:15 +0900

    [baekjoon] 소수의 연속합

--- baekjoon/1644.cpp
+++ baekjoon/1644.cpp
@@ -1,66 +1,26 @@
 // 1644. 소수의 연속합
-#include<cstdio>
-#include<vector>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int n;
-
-long long a[4000005];
+int n,ret,s,e,sum;
+bool arr[4000005];
 vector<int> v;
-
-int cnt=0;
-
-//에라토스테네스의 체
-void eratosthenes() {
-    a[0]=a[1]=-1;
-
-    for(long long i=2;i<4000000;i++) {
-        a[i]=i;
-    }
-
-    for(long long i=2;i<4000000;i++) {
-        if(a[i]==i) { 
-            for(long long j=i*i; j<4000000; j+=i) {
-                if (a[j]==j) a[j]=i;
-            }
-        }
-    }
-    for(int i=2;i<4000000;i++) {
-        if(a[i]==i) v.push_back(i);
-    }   
-}
-
-void func() {
-    int low=0,high=0;
-    int size=v.size();
-    int sum=v[0];
-
-    while(high<size && v[low]<=n) {
-        if(sum<n) {
-            sum+=v[++high];
-        } else if(sum==n) {
-            cnt++;
-            sum+=v[++high];
-        } else if(sum>n) {
-            sum-=v[low++];
-
-            if(low>high && low<size) {
-                high=low;
-                sum=v[high];
-            }
-        }
-    }
-
-    printf("%d\n",cnt);
-}
-
 int main() {
-
-    scanf("%d",&n);
-
-    eratosthenes();
-    func();
-
-    return 0;
+	scanf("%d",&n);
+	for(int i=0;i<=n;i++) arr[i]=true;  
+	for(int i=2;i<=n;i++) {
+		if(arr[i]) {
+			for(int j=2;j<=n/i;j++){
+				if(arr[i*j]) arr[i*j]=false;
+			}
+		}
+	}
+	for(int i=2;i<=n;i++) if(arr[i]) v.push_back(i);
+	while(true) {
+		if(sum>=n) sum-=v[s++];
+		else if((int)v.size()==e) break;
+		else sum+=v[e++];
+		if(sum==n) ret++;
+	}
+	printf("%d\n",ret);
+	return 0;
 }
