e206527bbc344ccbfdaee56c676e5ce19611b3a2
양기현 <rlgusdid@naver.com>
2020-06-22 10:57:55 +0900

    [baekjoon] 회의실 배정

--- baekjoon/1931.cpp
+++ baekjoon/1931.cpp
@@ -1,22 +1,21 @@
 // 회의실 배정
 #include<bits/stdc++.h>
 using namespace std;
-int n,ret;
+int n,e,ret;
 vector<pair<int,int>> v;
 int main() {
 	scanf("%d",&n);
 	for(int i=0;i<n;i++) {
-		int s,e;		
-		scanf("%d %d",&s,&e);
-		v.push_back({e,s});
+		int a,b;
+		scanf("%d %d",&a,&b);
+		v.push_back({b,a});
 	}
 	sort(v.begin(),v.end());
-	int e=v[0].first;
-	ret++;
-	for(int i=1;i<n;i++) {
-		if(e>v[i].second) continue;
-		e=v[i].first;
-		ret++;
+	for(auto it:v) {
+		if(e<=it.second) {
+			e=it.first;
+			ret++;
+		}
 	}
 	printf("%d\n",ret);
 	return 0;
