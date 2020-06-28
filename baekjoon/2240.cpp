8b872388a5327e88e1e3240a58369507a5eeca9d
양기현 <rlgusdid@naver.com>
2020-06-28 23:44:49 +0900

    [baekjoon] 자두나무

--- baekjoon/2240.cpp
+++ baekjoon/2240.cpp
@@ -1,33 +1,24 @@
 // 자두나무
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int t,w;
-int arr[1010];
-int dp[3][1010][35];
-
-// 현재 나무에서 특정 시간동안 특정거리만큼 움직였을 때 얻을 수 있는 자두의 최대개수를 반환하는 함수
-int dfs(int current_tree,int time,int dist) {
-    if(time==t) return 0;
-    if(dp[current_tree][time][dist]!=-1) return dp[current_tree][time][dist];
-    
-    int& ret=dp[current_tree][time][dist];
-    int cnt=(arr[time]==current_tree);
-    int next_tree=(current_tree==1)?2:1;
-
-    if(w>dist) ret=max(dfs(next_tree,time+1,dist+1)+!cnt,dfs(current_tree,time+1,dist)+cnt);
-    else ret=dfs(current_tree,time+1,dist)+cnt;
-    return ret;
+const int INF=987654321;
+int t,w,arr[1005],dp[1005][35][3];
+int dfs(int time,int hp,int cur_tree) {
+	if(time==t) return 0;
+	int& ret=dp[time][hp][cur_tree];
+	if(ret!=-1) return ret;
+	int next_tree=(cur_tree==1)?2:1;
+	int val=(arr[time]==cur_tree)?1:0;
+	if(hp>0) {
+		ret=max(ret,dfs(time+1,hp-1,next_tree)+!val);
+		ret=max(ret,dfs(time+1,hp,cur_tree)+val);
+	} else ret=max(ret,dfs(time+1,hp,cur_tree)+val);
+	return ret;
 }
-
 int main() {
-
-    scanf("%d %d",&t,&w);
-    for(int i=0;i<t;i++) scanf("%d",&arr[i]);
-    
-    fill(&dp[0][0][0],&dp[0][0][0]+3*1010*35,-1);
-    printf("%d\n",dfs(1,0,0));      // 나무(1,2), 시간, 이동가능 거리
-
-    return 0;
+	scanf("%d %d",&t,&w);
+	for(int i=0;i<t;i++) scanf("%d",&arr[i]);
+	memset(dp,-1,sizeof(dp));
+	printf("%d\n",dfs(0,w,1));
+	return 0;
 }
