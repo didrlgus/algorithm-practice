666f10e245618af7b4d0baecb04960c721fdae84
양기현 <rlgusdid@naver.com>
2020-06-28 22:29:39 +0900

    [baekjoon] 동전 2

--- baekjoon/2294.cpp
+++ baekjoon/2294.cpp
@@ -2,20 +2,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int INF=987654321;
-int n,k,coin[110],dp[100010];
+int n,k,coin[105],dp[100005];
 int main() {
-    scanf("%d %d",&n,&k);
-    fill(dp,dp+10010,INF);
-    for(int i=0;i<n;i++) {
-        scanf("%d",&coin[i]);
-        dp[coin[i]]=1;
-    }
-    for(int i=1;i<=k;i++) {
-        for(int j=0;j<n;j++) {
-            if(i-coin[j]>=0) dp[i]=min(dp[i],dp[i-coin[j]]+1);
-        }
-    }
-    if(dp[k]==INF) printf("-1\n");
-    else printf("%d\n",dp[k]);
-    return 0;
+	scanf("%d %d",&n,&k);
+	fill(dp,dp+100005,INF);
+	for(int i=0;i<n;i++) {
+		scanf("%d",&coin[i]);
+		dp[coin[i]]=1;
+	}
+	for(int i=0;i<=k;i++) {
+		for(int j=0;j<n;j++) if(i-coin[j]>=0) dp[i]=min(dp[i],dp[i-coin[j]]+1);
+	}
+	printf("%d\n",dp[k]==INF?-1:dp[k]);
+	return 0;
 }
