ebb41c66b09f43fafd0b440aa89854439808bb16
양기현 <rlgusdid@naver.com>
2020-07-24 16:37:58 +0900

    [baekjoon] 나는야 포켓몬 마스터 이다솜

--- baekjoon/1620.cpp
+++ baekjoon/1620.cpp
@@ -1,36 +1,22 @@
 // 나는야 포켓몬 마스터 이다솜
 #include<bits/stdc++.h>
-
 using namespace std;
-
 int n,m;
-string arr[100010];
+vector<string> v;
 unordered_map<string,int> mp;
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin>>n>>m;
-
-    for(int i=1;i<=n;i++) {
-        string s;
-        cin>>s;
-        arr[i]=s;
-        mp[s]=i;
-    }
-
-    for(int i=0;i<m;i++) {
-        string s;
-        cin>>s;
-        
-        if(s[0]>='0' && s[0]<='9') {
-            cout<<arr[atoi(s.c_str())]<<"\n";
-        } else {
-            cout<<mp[s]<<"\n";
-        }
-    }
-
-    return 0;
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	cin>>n>>m;
+	for(int i=1;i<=n;i++) {
+		string s;cin>>s;
+		v.push_back(s);
+		mp[s]=i;
+	}
+	while(m--) {
+		string s;cin>>s;
+		int a=atoi(s.c_str());
+		if(a) cout<<v[a-1]<<'\n';
+		else cout<<mp[s]<<'\n';
+	}
+	return 0;
 }
