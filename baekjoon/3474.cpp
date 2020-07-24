b7fcbc5c5bc841de1b87363b0e743aafd4056e63
양기현 <rlgusdid@naver.com>
2020-07-24 16:14:57 +0900

    [baekjoon] 교수가 된 현우

--- baekjoon/3474.cpp
+++ baekjoon/3474.cpp
@@ -1,28 +1,21 @@
 // 교수가 된 현우
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n;
+int tc,n;
 int main() {
-
-    scanf("%d",&n);
-
-    for(int i=0;i<n;i++) {
-        int a;
-        scanf("%d",&a);
-
-        int cnt2=0,cnt5=0;
-        for(int j=2;j<=a;j*=2) {       // 2,2^2,2^3... 으로 나눈 몫의 총합 구하기
-            cnt2+=(a/j);
-        }
-
-        for(int j=5;j<=a;j*=5) {
-            cnt5+=(a/j);
-        }
-
-        printf("%d\n",min(cnt2,cnt5));
-    }
-
-    return 0;
+	scanf("%d",&tc);
+	while(tc--) {
+		scanf("%d",&n);
+		int a=2,b=5,cnt2=0,cnt5=0;
+		while(a<=n) {
+			cnt2+=n/a;
+			a*=2;
+		}
+		while(b<=n) {
+			cnt5+=n/b;
+			b*=5;
+		}
+		printf("%d\n",min(cnt2,cnt5));
+	}
+	return 0;
 }
