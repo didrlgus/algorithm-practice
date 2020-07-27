d29df653b67f2fd56d1bfb259a6c8085ed7a01b4
양기현 <rlgusdid@naver.com>
2020-07-27 17:09:09 +0900

    [baekjoon] 폴리오미노

--- baekjoon/1343.cpp
+++ baekjoon/1343.cpp
@@ -1,67 +1,42 @@
 // 폴리오미노
 #include<bits/stdc++.h>
-
 using namespace std;
-
-string s;
+int n;
+string s,str;
+bool check(int idx,int type) {
+	for(int i=idx;i<idx+type;i++) {
+		if(s[i]!='X') return false;
+	}
+	return true;
+}
+void go(int here) {
+	if(here>=n) {
+		cout<<str<<'\n';
+		exit(0);
+		return;
+	}
+	string tmp=str;
+	if(s[here]=='.') {
+		str+='.';
+		go(here+1);
+		str=tmp;
+	} else {
+		if(check(here,4)) {
+			str+="AAAA";
+			go(here+4);
+			str=tmp;
+		} else if(check(here,2)) {
+			str+="BB";
+			go(here+2);
+			str=tmp;
+		}
+	}
+}
 int main() {
-
-    cin>>s;
-
-    if(s.size()<2) {
-        if(s==".") cout<<s<<endl;
-        else cout<<-1<<endl;
-        return 0;
-    } else if(s.size()<4) {   // size 2 or 3
-        if(s==".." || s=="...") {
-            cout<<s<<endl;
-            return 0;
-        }
-        if(s.size()==3 && s[0]=='X' && s[1]=='X' && s[2]=='X') {
-            cout<<-1<<endl;
-            return 0;
-        }
-        bool flag=false;
-        for(int i=0;i<=s.size()-2;i++) {
-            if(s[i]=='X' && s[i+1]=='X') {
-                s[i]='B';
-                s[i+1]='B';
-                flag=true;
-            }
-        }
-        if(!flag) cout<<-1<<endl;
-        else cout<<s<<endl;;
-        return 0;
-    }
-
-    // AAAA 채우기
-    for(int i=0;i<=s.size()-4;i++) {
-        if(s[i]=='X' && s[i+1]=='X' && s[i+2]=='X' && s[i+3]=='X') {
-            if((i>0 && s[i-1]!='X') || i==0) {
-                s[i]='A'; s[i+1]='A'; s[i+2]='A'; s[i+3]='A';
-                i+=3;
-            }
-        }
-    }
-
-    // BB 채우기
-    for(int i=0;i<=s.size()-2;i++) {
-        if(s[i]=='X' && s[i+1]=='X') {
-            if((i>0 && s[i-1]!='X') || i==0) {
-                s[i]='B'; s[i+1]='B';
-                i+=1;
-            }
-        }
-    }
-
-    for(int i=0;i<s.size();i++) {
-        if(s[i]=='X') {
-            cout<<-1<<endl;
-            return 0;
-        }
-    }
-
-    cout<<s<<endl;
-
-    return 0;
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	cin>>s;
+	n=(int)s.size();
+	go(0);
+	printf("-1\n");
+	return 0;
 }
