f3ed609325d64f2478b9fcb07eb114f3953918f4
양기현 <rlgusdid@naver.com>
2020-07-27 19:56:32 +0900

    [baekjoon] 덩치

--- baekjoon/7568.cpp
+++ baekjoon/7568.cpp
@@ -1,32 +1,21 @@
-// 7568. 덩치
+// 덩치
 #include<bits/stdc++.h>
-
 using namespace std;
-
 int n;
 vector<pair<int,int>> v;
 int main() {
-
-    scanf("%d",&n);
-
-    for(int i=0;i<n;i++) {
-        int a,b;
-        scanf("%d %d",&a,&b);
-
-        v.push_back({a,b});
-    }
-
-    for(int i=0;i<v.size();i++) {           // 내 몸무게
-        int cnt=0;
-        for(int j=0;j<v.size();j++) {       // 상대 몸무게
-            if(i==j) continue;
-            
-            int x=v[i].first,y=v[i].second,p=v[j].first,q=v[j].second;
-
-            if(x<p && y<q) cnt++;
-        }
-        printf("%d ",++cnt);
-    }
-
-    return 0;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++) {
+		int a,b;scanf("%d%d",&a,&b);
+		v.push_back({a,b});
+	}
+	for(int i=0;i<n;i++) {
+		int cnt=0;
+		for(int j=0;j<n;j++) {
+			if(i==j) continue;
+			if(v[i].first<v[j].first&&v[i].second<v[j].second) cnt++;
+		}
+		printf("%d ",cnt+1);
+	}
+	return 0;
 }
