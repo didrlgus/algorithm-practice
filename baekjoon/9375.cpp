1f687e4ff57cbc6cee3bc73460affa67e9fd8e6a
양기현 <rlgusdid@naver.com>
2020-07-27 19:34:50 +0900

    [baekjoon] 패션왕 신해빈

--- baekjoon/9375.cpp
+++ baekjoon/9375.cpp
@@ -1,39 +1,24 @@
-// 9375. 패션왕 신해빈
+// 패션왕 신해빈
 #include<bits/stdc++.h>
-
 using namespace std;
-
-map<string,int> m;
-
+int tc,n;
+map<string, int> mp;
+set<string> st;
 int main() {
-
-    int n;
-    scanf("%d",&n);
-
-    while(n--) {
-        int a;
-        scanf("%d\n",&a);
-
-        while(a--) {
-            string s;
-            getline(cin,s);
-
-            int idx=s.find(" ");
-
-            string ns="";
-            for(int i=idx+1;i<s.size();i++) ns+=s[i];
-
-            m[ns]++;
-        }
-        
-        int sum=1;
-        for(auto iter:m) {
-            sum*=(iter.second+1);       // (각각 입는 경우 + 아무것도 안 입는 경우)
-        }
-        sum--;                          // 모두 입지 않은 경우 -1
-        printf("%d\n",sum);
-        m.clear();
-    }
-
-    return 0;
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	cin>>tc;
+	while(tc--) {
+		mp.clear();st.clear();
+		cin>>n;
+		int ret=1;
+		for(int i=0;i<n;i++) {
+			string s1,s2;
+			cin>>s1;cin>>s2;
+			st.insert(s2);
+			mp[s2]++;
+		}
+		for(auto it:st) ret*=(mp[it]+1);
+		cout<<ret-1<<'\n';
+	}
+	return 0;
 }
