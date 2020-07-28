b433fac11b7258f33e92e940f6beaf972316d2fc
양기현 <rlgusdid@naver.com>
2020-07-28 13:45:29 +0900

    [baekjoon] 종이자르기

--- baekjoon/2628.cpp
+++ baekjoon/2628.cpp
@@ -1,86 +1,43 @@
 // 종이자르기
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n,m,k;
+const int INF=-987654321;
+int n,m,k,r_cnt,c_cnt,ret=INF,dy[]={0,0,1,-1},dx[]={1,-1,0,0};
+bool arr[205][205],visited[205][205];
 vector<pair<int,int>> v;
-int arr[210][210];
-int visited[210][210];
-
-queue<pair<int,int>> q;
-
-int dy[]={0,0,1,-1},dx[]={1,-1,0,0};
-
-int max_=-987987987;
-
-int bfs(int y,int x) {
-    // 시작점 방문
-    visited[y][x]=1;
-    q.push({y,x});
-
-    int sum=1;
-    while(!q.empty()) {
-        tie(y,x)=q.front();
-        q.pop();
-        
-        for(int i=0;i<4;i++) {
-            int ny=y+dy[i],nx=x+dx[i];
-
-            if(ny<0 || ny>=n || nx<0 || nx>=m) continue;
-
-            if(!visited[ny][nx] && arr[ny][nx]==0) {
-                visited[ny][nx]=visited[ny][nx]+1;
-                q.push({ny,nx});
-                sum++;
-            }
-        }
-    }
-    return sum;
+int dfs(int y,int x) {
+	visited[y][x]=true;
+	int ret=0;
+	for(int i=0;i<4;i++) {
+		int ny=y+dy[i],nx=x+dx[i];
+		if(ny<0||nx<0||ny>=n+r_cnt||nx>=m+c_cnt) continue;
+		if(!visited[ny][nx]&&!arr[ny][nx]) ret+=dfs(ny,nx)+1;
+	}
+	return ret;
 }
-
 int main() {
-
-    scanf("%d %d",&m,&n);
-    scanf("%d",&k);
-
-    for(int i=0;i<k;i++) {
-        int a,b;
-        scanf("%d %d",&a,&b);
-        v.push_back({a,b});
-    }
-
-    sort(v.begin(),v.end());
-
-    for(int i=0;i<v.size();i++) {
-        if(v[i].first==0) {         // 가로 자르기
-            int rCnt=0,rIdx=0;
-            while(true) {
-                if(arr[rIdx++][0]==0) rCnt++;
-                if(rCnt>=v[i].second) break;
-            }
-            n++;
-            for(int j=0;j<210;j++) arr[rIdx][j]=1;
-        } else {                    // 세로 자르기
-            int cCnt=0,cIdx=0;
-            while(true) {
-                if(arr[0][cIdx++]==0) cCnt++;
-                if(cCnt>=v[i].second) break;
-            }
-            m++;
-            for(int j=0;j<210;j++) arr[j][cIdx]=1;
-        }
-    }
-
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<m;j++) {
-            if(!visited[i][j] && arr[i][j]==0){
-                max_=max(max_,bfs(i,j));
-            }
-        }
-    }
-
-    printf("%d\n",max_);
-
-    return 0;
+	scanf("%d%d%d",&m,&n,&k);
+	for(int i=0;i<k;i++) {
+		int a,b;scanf("%d%d",&a,&b);
+		v.push_back({a,b});
+	}
+	sort(v.begin(),v.end());
+	for(auto it:v) {
+		if(!it.first) {
+			for(int j=0;j<205;j++) arr[it.second+r_cnt][j]=true;
+			r_cnt++;
+		} else {
+			for(int i=0;i<205;i++) arr[i][it.second+c_cnt]=true;
+			c_cnt++;
+		}
+	}
+	for(int i=0;i<n+r_cnt;i++) {
+		for(int j=0;j<m+c_cnt;j++) {
+			if(!visited[i][j]&&!arr[i][j]) {
+				ret=max(ret,dfs(i,j)+1);
+			}
+		}
+	}
+	printf("%d\n",ret);
+	return 0;
 }
