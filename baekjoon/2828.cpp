0a94e44c35c501f0d3571dc99ac9a20b2e610428
양기현 <rlgusdid@naver.com>
2020-07-28 15:41:03 +0900

    [baekjoon] 사과 담기 게임

--- baekjoon/2828.cpp
+++ baekjoon/2828.cpp
@@ -1,34 +1,22 @@
 // 사과 담기 게임
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n,m,k;
+int n,m,k,s,e,ret;
 int main() {
-
-    scanf("%d %d",&n,&m);
-    scanf("%d",&k);
-
-    int s=0,e=m;
-    int cnt=0;
+    scanf("%d%d%d",&n,&m,&k);
+    s=0,e=m;
     for(int i=0;i<k;i++) {
-        int a;
-        scanf("%d",&a);
-
-        if(a<=s) {          // 왼쪽으로 이동
-            int d=s-a+1;
-            s-=d;
-            e-=d;
-            cnt+=d;
-        } else if(a>e) {    // 오른쪽으로 이동
-            int d=a-e;
-            s+=d;
-            e+=d;
-            cnt+=d;
-        }
+        int a;scanf("%d",&a);
+        if(a>e) {
+            ret+=a-e;
+            e=a;
+            s=e-m;
+        } else if(a<=s) {
+            ret+=s-a+1;
+            s=a-1;
+            e=s+m;
+        }        
     }
-
-    printf("%d\n",cnt);
-
+    printf("%d\n",ret);
     return 0;
 }
