93f25f61a222a518d70a7680d32ee98633c6671b
양기현 <rlgusdid@naver.com>
2020-07-30 23:58:44 +0900

    [baekjoon] 숨바꼭질 4

--- baekjoon/13913.cpp
+++ baekjoon/13913.cpp
@@ -1,62 +1,41 @@
-// 13913. 숨바꼭질 4
+// 숨바꼭질 4
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int a,b;
-bool visited[100005];
-int parent[100005];
+int n,k,dm[]={1,1,2},da[]={1,-1,0},parent[100005],level;
+bool visited[100005],flag;
 queue<int> q;
-
-int level;
-vector<int> v;
-void bfs(int y) {
-    visited[y]=true;
-    q.push(y);
-
+void go(int y) {
+    if(y==-1) return;
+    go(parent[y]);
+    printf("%d ",y);
+}
+int main() {
+    scanf("%d%d",&n,&k);
+    q.push(n);
+    visited[n]=true;
+    parent[n]=-1;
     while(!q.empty()) {
-        int size=q.size();
+        int size=(int)q.size();
         while(size--) {
-            y=q.front(); q.pop();
-
-            if(y==b) {
-                printf("%d\n",level);
-                int cur=b;
-                v.push_back(b);
-                while(cur!=a) {             // 최상위 노드가 아닐때까지
-                    v.push_back(parent[cur]);
-                    cur=parent[cur];
-                }
-
-                for(int i=v.size()-1;i>=0;i--) printf("%d ",v[i]);
-                return;
+            int cur=q.front();q.pop();
+            if(cur==k) {
+                flag=true;
+                break;
             }
-
-            if(y-1>=0 && !visited[y-1]) {
-                visited[y-1]=true;
-                parent[y-1]=y;
-                q.push(y-1);
-            }
-            if(y+1<=100000 && !visited[y+1]) {
-                visited[y+1]=true;
-                parent[y+1]=y;
-                q.push(y+1);
-            }
-            if(y*2<=100000 && !visited[y*2]) {
-                visited[y*2]=true;
-                parent[y*2]=y;
-                q.push(y*2);
+            for(int i=0;i<3;i++) {
+                int next=cur*dm[i]+da[i];
+                if(next<0||next>100000) continue;
+                if(!visited[next]) {
+                    parent[next]=cur;
+                    visited[next]=true;
+                    q.push(next);
+                } 
             }
         }
+        if(flag) break;
         level++;
     }
-}
-
-
-int main() {
-    scanf("%d %d",&a,&b);
-
-    bfs(a);
-
+    printf("%d\n",level);
+    go(k);
     return 0;
 }
