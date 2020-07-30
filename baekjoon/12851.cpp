1a1b87db45a88366f044a56e52fbac0d7cb49e9c
양기현 <rlgusdid@naver.com>
2020-07-30 23:33:26 +0900

    [baekjoon] 숨바꼭질 2

--- baekjoon/12851.cpp
+++ baekjoon/12851.cpp
@@ -1,46 +1,29 @@
-// 12851. 숨바꼭질 2
+// 숨바꼭질 2
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int a,b;
-bool visited[100010];
+int n,k,dm[]={1,1,2},da[]={1,-1,0},ret;
+bool visited[100005];
 queue<int> q;
-
-int level,cnt;
-void bfs(int y) {
-    visited[y]=true;
-    q.push(y);
-
+int main() {
+    scanf("%d%d",&n,&k);
+    q.push(n);
+    visited[n]=true;
+    int level=0;
     while(!q.empty()) {
-        int size=q.size();
-
+        int size=(int)q.size();
         while(size--) {
-            y=q.front();
-            q.pop();
-            visited[y]=true;                    // 큐에서 뽑은 다음 바로 경로 저장, 경로를 이부분에서 true시켜주는 예외적인 상황
-
-            if(y==b) cnt++;
-
-            if(y+1<=100000 && !visited[y+1]) {
-                q.push(y+1);
-            }
-            if(y-1>=0 && !visited[y-1]) {
-                q.push(y-1);
-            }
-            if(y*2<=100000 && !visited[y*2]) {
-                q.push(y*2);
+            int cur=q.front();q.pop();
+            visited[cur]=true;
+            if(cur==k) ret++;
+            for(int i=0;i<3;i++) {
+                int next=cur*dm[i]+da[i];
+                if(next<0||next>100000) continue;
+                if(!visited[next]) q.push(next);
             }
         }
-        if(cnt>0) break;
+        if(ret) break;
         level++;
     }
-}
-
-int main() {
-    scanf("%d %d",&a,&b);
-
-    bfs(a);
-    printf("%d\n%d\n",level,cnt);
+    printf("%d\n%d\n",level,ret);
     return 0;
 }
