0131893c406318fec6c7adab3b49001ff0c7937d
양기현 <rlgusdid@naver.com>
2020-07-31 13:25:33 +0900

    [baekjoon] 주몽

--- baekjoon/1940.cpp
+++ baekjoon/1940.cpp
@@ -1,40 +1,17 @@
-// 주몽
+// ??
 #include<bits/stdc++.h>
-
 using namespace std;
-
-vector<int> v1,v2;
-
-int cnt=0;
-int n,k;
-void func(int here,vector<int> &v2) {
-    if(v2.size()==2) {
-        int sum=0;
-        for(auto iter:v2) sum+=v1[iter];
-        
-        if(sum==k) cnt++;
-
-        return;
-    }
-    for(int i=here+1;i<v1.size();i++) {
-        v2.push_back(i);
-        func(i,v2);
-        v2.pop_back();
-    }
-}
-
+int n,m,arr[15005],lo,hi,ret;
 int main() {
-
-    scanf("%d %d",&n,&k);
-
-    for(int i=0;i<n;i++) {
-        int a;
-        scanf("%d",&a);
-        v1.push_back(a);
+    scanf("%d%d",&n,&m);
+    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
+    sort(arr,arr+n);
+    lo=0,hi=n-1;
+    while(lo<hi) {
+        if(arr[lo]+arr[hi]==m) ret++,lo++;
+        else if(arr[lo]+arr[hi]<m) lo++;
+        else hi--; 
     }
-
-    func(-1,v2);
-    printf("%d\n",cnt);
-
+    printf("%d\n",ret);
     return 0;
 }
