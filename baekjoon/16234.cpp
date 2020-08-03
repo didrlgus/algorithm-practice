cfae2b677c494201b430d57b75b1afc4285f9073
양기현 <rlgusdid@naver.com>
2020-08-03 15:40:36 +0900

    [baekjoon] 인구 이동

--- baekjoon/16234.cpp
+++ baekjoon/16234.cpp
@@ -1,76 +1,52 @@
 // 인구 이동
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n,l,r;
-int arr[55][55];
-
-int dy[]={0,0,1,-1};
-int dx[]={1,-1,0,0};
-queue<pair<int,int>> q;
-vector<vector<pair<int,int>>> v;
-bool visited[55][55];
-
-vector<pair<int,int>> bfs(int y,int x) {
-    vector<pair<int,int>> u;            // 연합의 좌표
+int n,l,r,arr[55][55],tmp[55][55],dy[]={0,0,1,-1},dx[]={1,-1,0,0},sum,ret;
+bool visited[55][55],flag;
+vector<pair<int,int>> v;
+void dfs(int y,int x) {
     visited[y][x]=true;
-    q.push({y,x});
-    u.push_back({y,x});
-
-    while(!q.empty()) {
-        tie(y,x)=q.front();
-        q.pop();
-
-        for(int i=0;i<4;i++) {
-            int ny=y+dy[i],nx=x+dx[i];
-
-            if(ny<0||nx<0||ny>=n||nx>=n) continue;
-            if(!visited[ny][nx] && (abs(arr[y][x]-arr[ny][nx])>=l && abs(arr[y][x]-arr[ny][nx])<=r)) {
-                visited[ny][nx]=true;
-                q.push({ny,nx});
-                u.push_back({ny,nx});
+    v.push_back({y,x});
+    for(int i=0;i<4;i++) {
+        int ny=y+dy[i],nx=x+dx[i];
+        if(ny<0||nx<0||ny>=n||nx>=n) continue;
+        if(!visited[ny][nx]) {
+            int diff=abs(arr[y][x]-arr[ny][nx]);
+            if(diff>=l&&diff<=r) {
+                sum+=arr[ny][nx];
+                dfs(ny,nx);
             }
         }
     }
-
-    return u;
 }
-
 int main() {
-
-    scanf("%d %d %d",&n,&l,&r);
-
+    scanf("%d%d%d",&n,&l,&r);
     for(int i=0;i<n;i++) {
-        for(int j=0;j<n;j++) {
-            scanf("%d",&arr[i][j]);
-        }
+        for(int j=0;j<n;j++) scanf("%d",&arr[i][j]);
     }
-
-    int t=0;
     while(true) {
+        v.clear();
+        memset(visited,0,sizeof(visited));
+        memcpy(tmp,arr,sizeof(arr));
+        flag=false;
         for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {   
-                vector<pair<int,int>> r=bfs(i,j);
-
-                if(r.size()>1) v.push_back(r);      // 연합이 있는 경우 v에 저장
+            for(int j=0;j<n;j++) {
+                if(!visited[i][j]) {
+                    sum=0;
+                    dfs(i,j);
+                    if((int)v.size()>1) {
+                        int avg=(sum+arr[i][j])/(int)v.size();
+                        flag=true;
+                        for(auto it:v) tmp[it.first][it.second]=avg;
+                    }
+                    v.clear();
+                }
             }
         }
-        if(v.size()==0) break;                      // 연합이 없다면 break
-        t++;
-
-        for(int i=0;i<v.size();i++) {
-            int sum=0,avg=0;
-            for(auto iter:v[i]) sum+=arr[iter.first][iter.second];
-            avg=sum/v[i].size();
-            for(auto iter:v[i]) arr[iter.first][iter.second]=avg;
-        }
-
-        fill(&visited[0][0],&visited[0][0]+55*55,0);
-        v.clear();     
+        if(!flag) break;
+        memcpy(arr,tmp,sizeof(tmp));
+        ret++;
     }
-        
-    printf("%d\n",t);
-
+    printf("%d\n",ret);
     return 0;
 }
