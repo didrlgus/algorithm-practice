0b267a8644e0535a2db5b0a9ccca5f99546b6d0e
양기현 <rlgusdid@naver.com>
2020-08-04 16:40:26 +0900

    [baekjoon] 치킨 배달

--- baekjoon/15686.cpp
+++ baekjoon/15686.cpp
@@ -1,57 +1,40 @@
 // 치킨 배달
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n,m;
-int arr[55][55];
-
-vector<pair<int,int>> v1,v2;    // 모든 치킨집 좌표, 집 좌표
-vector<int> v3;                 // 선택한 치킨집 좌표
-int result=987654321;
-
-int getDis(int y1,int x1,int y2,int x2) {
-    int dis=0;
-    dis=abs(y1-y2)+abs(x1-x2);
-    return dis;
+const int INF=987654321;
+int n,m,arr[55][55],ret=INF;
+vector<int> v,v2,home;
+int getDist(int sy,int sx,int ey,int ex) {
+    return abs(sy-ey)+abs(sx-ex);
 }
-
-void func(int here,vector<int>& v3) {
-    if(v3.size()==m) {
-        int ret=0;
-        for(int i=0;i<v2.size();i++) {
-            int mn=987654321;            
-            int hy=v2[i].first,hx=v2[i].second;
-
-            for(auto iter:v3) {      // m개 선택한 치킨집의 좌표
-                int cy=v1[iter].first,cx=v1[iter].second;
-                mn=min(mn,getDis(hy,hx,cy,cx));
-            }
-            ret+=mn;
+void combi(int here) {
+    if((int)v2.size()==m) {
+        int tot_dist=0;
+        for(auto it:home) {
+            int dist=INF;
+            int sy=it/100,sx=it%100;
+            for(auto it2:v2) dist=min(dist,getDist(sy,sx,v[it2]/100,v[it2]%100));
+            tot_dist+=dist;
         }
-        result=min(result,ret);
+        ret=min(ret,tot_dist);
+        return;
     }
-    for(int i=here+1;i<v1.size();i++) {
-        v3.push_back(i);
-        func(i,v3);
-        v3.pop_back();
+    for(int i=here+1;i<(int)v.size();i++) {
+        v2.push_back(i);
+        combi(i);
+        v2.pop_back();
     }
 }
-
 int main() {
-    scanf("%d %d",&n,&m);
-
+    scanf("%d%d",&n,&m);
     for(int i=0;i<n;i++) {
         for(int j=0;j<n;j++) {
             scanf("%d",&arr[i][j]);
-
-            if(arr[i][j]==2) v1.push_back({i,j});
-            if(arr[i][j]==1) v2.push_back({i,j});
+            if(arr[i][j]==1) home.push_back(i*100+j);
+            if(arr[i][j]==2) v.push_back(i*100+j);
         }
     }
-
-    func(-1,v3);
-    printf("%d\n",result);
-
+    combi(-1);
+    printf("%d\n",ret);
     return 0;
 }
