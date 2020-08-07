a56896e1b2cf523761f0ae7ff28632a82c93ec2b
양기현 <rlgusdid@naver.com>
2020-08-07 16:05:59 +0900

    [baekjoon] 돌 게임 5

---
+++ baekjoon/9659.cpp
@@ -0,0 +1,11 @@
+// 돌 게임 5
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll n;
+int main() {
+    scanf("%lld",&n);
+    if(n&1) printf("SK\n");
+    else printf("CY\n");
+    return 0;
+}
