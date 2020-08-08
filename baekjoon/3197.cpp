41bf2c7e4c6c12e2bb7b9862dcb0389bc8a05189
양기현 <rlgusdid@naver.com>
2020-08-09 00:44:24 +0900

    [baekjoon] 백조의 호수

--- baekjoon/3197.cpp
+++ baekjoon/3197.cpp
@@ -1,88 +1,72 @@
 // 백조의 호수
 #include<bits/stdc++.h>
-
 using namespace std;
-
-int n,m;
-char arr[1510][1510];
-bool visited[1510][1510];
-
-int dy[]={0,0,1,-1};
-int dx[]={1,-1,0,0};
-
-queue<pair<int,int>> q,q2;          // 백조, 물 큐
-bool flag=false;
-
-int main() {
-
-    scanf("%d %d",&n,&m);
-
+int n,m,ret,sy,sx,ey,ex,dy[]={0,0,1,-1},dx[]={1,-1,0,0};
+char arr[1505][1505];
+vector<pair<int,int>> swan;
+bool water_visited[1505][1505],swan_visited[1505][1505];
+queue<pair<int,int>> water_q,swan_q;
+bool swan_move() {
     int y,x;
+    queue<pair<int,int>> tmp_q;
+    while(!swan_q.empty()) {
+        tie(y,x)=swan_q.front();swan_q.pop();
+        if(y==ey&&x==ex) return true;
+        for(int i=0;i<4;i++) {
+            int ny=y+dy[i],nx=x+dx[i];
+            if(ny<0||nx<0||ny>=n||nx>=m) continue;
+            if(!swan_visited[ny][nx]&&arr[ny][nx]=='.') {
+                swan_visited[ny][nx]=true;
+                swan_q.push({ny,nx});
+            }
+            if(!swan_visited[ny][nx]&&arr[ny][nx]=='X') {
+                swan_visited[ny][nx]=true;
+                tmp_q.push({ny,nx});
+            }
+        }
+    }
+    swan_q=tmp_q;
+    return false;
+}
+void water_spread() {
+    int y,x;
+    queue<pair<int,int>> tmp_q;
+    while(!water_q.empty()) {
+        tie(y,x)=water_q.front();water_q.pop();
+        for(int i=0;i<4;i++) {
+            int ny=y+dy[i],nx=x+dx[i];
+            if(ny<0||nx<0||ny>=n||nx>=m) continue;
+            if(!water_visited[ny][nx]&&arr[ny][nx]=='X') {
+                water_visited[ny][nx]=true;
+                tmp_q.push({ny,nx});
+                arr[ny][nx]='.';
+            }
+        }
+    }
+    water_q=tmp_q;
+}
+int main() {
+    scanf("%d%d",&n,&m);
     for(int i=0;i<n;i++) {
         for(int j=0;j<m;j++) {
             scanf(" %1c",&arr[i][j]);
-
             if(arr[i][j]=='L') {
-                y=i; x=j;                   // 백조의 위치 좌표
-                q2.push({i,j});             // 백조도 물로 취급
-            } else if(arr[i][j]=='.') {     // 물 미리 채워넣기
-                q2.push({i,j});
+                swan.push_back({i,j});
+                arr[i][j]='.';
+            }
+            if(arr[i][j]=='.') {
+                water_visited[i][j]=true;
+                water_q.push({i,j});
             }
         }
     }
-
-    int t=0;
-    visited[y][x]=true;
-    q.push({y,x});
+    swan_q.push({swan.front().first,swan.front().second});
+    ey=swan.back().first,ex=swan.back().second;
     while(true) {
-        // 백조 플러드 필
-        queue<pair<int,int>> tmp;
-        while(!q.empty()) {
-            tie(y,x)=q.front(); q.pop();
-
-            for(int i=0;i<4;i++) {
-                int ny=y+dy[i],nx=x+dx[i];
-                
-                if(ny<0||nx<0||ny>=n||nx>=m || visited[ny][nx]) continue;
-                
-                if(arr[ny][nx]=='.') {
-                    visited[ny][nx]=true;
-                    q.push({ny,nx});
-                } else if(arr[ny][nx]=='X') {
-                    visited[ny][nx]=true;
-                    tmp.push({ny,nx});
-                } else if(arr[ny][nx]=='L') {
-                    flag=true;
-                    break;
-                }
-            }
-            if(flag) break;
-        }
-        if(flag) break;
-        q=tmp;
-
-        // 물 플러드 필
-        queue<pair<int,int>> tmp2;
-        while(!q2.empty()) {
-            int y2,x2;
-            tie(y2,x2)=q2.front(); q2.pop();
-
-            for(int i=0;i<4;i++) {
-                int ny=y2+dy[i],nx=x2+dx[i];
-
-                if(ny<0||nx<0||ny>=n||nx>=m) continue;
-                if(arr[ny][nx]=='X') {
-                    tmp2.push({ny,nx});
-                    arr[ny][nx]='.';
-                }
-            }
-        }
-        q2=tmp2;
-
-        t++;
+        if(swan_move()) break;
+        water_spread();
+        ret++;
     }
-    
-    printf("%d\n",t);
-
+    printf("%d\n",ret);
     return 0;
 }
