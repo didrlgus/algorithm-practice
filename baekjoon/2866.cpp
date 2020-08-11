56e74a1efe614558dba2f584602c8c1c5956e062
양기현 <rlgusdid@naver.com>
2020-08-11 14:59:46 +0900

    [baekjoon] 문자열 잘라내기

---
+++ baekjoon/2866.cpp
@@ -0,0 +1,30 @@
+// 문자열 잘라내기
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,lo,hi,ret;
+char arr[1005][1005];
+bool check(int mid) {
+    set<string> st;
+    for(int j=0;j<m;j++) {
+        string s="";
+        for(int i=mid;i<n;i++) s+=arr[i][j];
+        if(st.find(s)!=st.end()) return true;
+        st.insert(s);
+    }
+    return false;
+}
+int main() {
+    scanf("%d%d",&n,&m);
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) scanf(" %1c",&arr[i][j]);
+    }
+    lo=0,hi=n-1;
+    // 처음으로 중복이 시작되는 지점 찾기
+    while(lo<=hi) {
+        int mid=(lo+hi)/2;
+        if(check(mid)) hi=mid-1;
+        else lo=mid+1;
+    }
+    printf("%d\n",hi);
+    return 0;
+}
