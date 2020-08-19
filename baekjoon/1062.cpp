c6e2d9059480e60bb9a6f7b28906b1eb760c362e
양기현 <rlgusdid@naver.com>
2020-08-19 21:06:14 +0900

    [baekjoon] 가르침

--- baekjoon/1062.cpp
+++ baekjoon/1062.cpp
@@ -1,53 +1,33 @@
 // 가르침
 #include<bits/stdc++.h>
 using namespace std;
-const int INF=-987654321;
-int n,k,ret=INF;
-vector<string> v;
-bool visited[30];
-int counting() {
-    int cnt=0;
-    for(string s:v) {
-        bool flag=false;
-        for(int i=4;i<(int)s.size()-4;i++) {
-            if(!visited[s[i]-'a']) {
-                flag=true;
-                break;
-            }
-        }
-        if(!flag) cnt++;
-    }
-    return cnt;
-}
-void combi(int here,int cnt) {
+int n,k,words[55];
+int go(int here,int cnt,int teach) {
     if(cnt==k) {
-        ret=max(ret,counting());
-        return;
+        int cnt=0;
+        for(int i=0;i<n;i++) if(!(words[i]&~teach)) cnt++;
+        return cnt;
     }
+    int ret=-1;
     for(int i=here+1;i<26;i++) {
-        if(visited[i]) continue;
-        visited[i]=true;
-        combi(i,cnt+1);
-        visited[i]=false;
+        if(i==('a'-'a')||i==('n'-'a')||i==('t'-'a')||i==('i'-'a')||i==('c'-'a')) continue;
+        ret=max(ret,go(i,cnt+1,teach|(1<<i)));
     }
+    return ret;
 }
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);cout.tie(NULL);
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     cin>>n>>k;
     for(int i=0;i<n;i++) {
-        string s;
-        cin>>s;
-        v.push_back(s);
+        string s;cin>>s;
+        for(auto c:s) words[i]|=(1<<(c-'a'));
     }
-    visited['a'-'a']=true;visited['n'-'a']=true;
-    visited['t'-'a']=true;visited['i'-'a']=true;visited['c'-'a']=true;
-    k-=5;
-    if(k<0) {
-        printf("0\n");
+    if(k<5) {
+        cout<<0<<'\n';
         return 0;
     }
-    combi(-1,0);
-    printf("%d\n",ret);
+    k-=5;
+    int teach=(1<<'a'-'a')|(1<<'n'-'a')|(1<<'t'-'a')|(1<<'i'-'a')|(1<<'c'-'a');
+    cout<<go(-1,0,teach)<<'\n';
     return 0;
 }
