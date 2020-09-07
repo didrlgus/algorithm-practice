b1f66dad42258012ed151d017cec9d0f2fa1375e
양기현 <rlgusdid@naver.com>
2020-09-07 11:05:34 +0900

    [baekjoon] 최소 힙

---
+++ baekjoon/1927.cpp
@@ -0,0 +1,18 @@
+// 최소 힙
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+priority_queue<int,vector<int>,greater<int>> pq;
+int main() {
+    scanf("%d",&n);
+    while(n--) {
+        int a;scanf("%d",&a);
+        if(a==0) {
+            if(!pq.empty()) {
+                printf("%d\n",pq.top());
+                pq.pop();
+            } else printf("0\n"); 
+        } else pq.push(a);
+    }
+    return 0;
+}
