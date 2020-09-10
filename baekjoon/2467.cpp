196f7d6d39523e1bd8a010469576372cad3fc0dd
양기현 <rlgusdid@naver.com>
2020-09-10 17:42:36 +0900

    [baekjoon] 용액

---
+++ baekjoon/2467.cpp
@@ -0,0 +1,22 @@
+// 용액
+#include<bits/stdc++.h>
+using namespace std;
+const int INF=2e9;
+int n,arr[100005],lo,hi,ret_lo,ret_hi,ret=INF;
+int main() {
+    scanf("%d",&n);
+    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
+    lo=0,hi=n-1;
+    while(lo<hi) {
+        int sum=arr[lo]+arr[hi];
+        if(abs(sum)<ret) {
+            ret=abs(sum);
+            ret_lo=lo;
+            ret_hi=hi;
+        }
+        if(sum<0) lo++;
+        else hi--;
+    }
+    printf("%d %d\n",arr[ret_lo],arr[ret_hi]);
+    return 0;
+}
