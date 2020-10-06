8f3b93a7764853709f4690b4cfa56cf34ade59e1
양기현 <rlgusdid@naver.com>
2020-10-06 11:58:32 +0900

    [baekjoon] 강의실 배정

--- baekjoon/11000.cpp
+++ baekjoon/11000.cpp
@@ -3,23 +3,19 @@
 using namespace std;
 int n;
 vector<pair<int,int>> v;
-priority_queue<int,vector<int>,greater<int>> pq;
+priority_queue<int,vector<int>,greater<>> pq;
 int main() {
     scanf("%d",&n);
-    while(n--) {
-        int s,e;
-        scanf("%d %d",&s,&e);
+    for(int i=0;i<n;i++) {
+        int s,e;scanf("%d%d",&s,&e);
         v.push_back({s,e});
     }
     sort(v.begin(),v.end());
     pq.push(v[0].second);
     for(int i=1;i<(int)v.size();i++) {
-        if(pq.top()>v[i].first) pq.push(v[i].second);
-        else {
-            pq.pop();
-            pq.push(v[i].second);
-        }
+        if(pq.top()<=v[i].first) pq.pop();
+        pq.push(v[i].second);
     }
-    printf("%d\n",pq.size());
+    printf("%d\n",(int)pq.size());
     return 0;
 }
