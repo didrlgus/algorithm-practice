c961ffeeb7df33421119d7e6c591623c3e3d36cc
양기현 <rlgusdid@naver.com>
2020-10-17 18:28:28 +0900

    [baekjoon] 괄호의 값

---
+++ baekjoon/2504.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    private static Stack<String> stk = new Stack<>();
+    private static boolean flag=false;
+    private static int ret=0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str=sc.nextLine();
+        for(int i=0;i<str.length();i++) {
+            if(str.charAt(i)=='(') {
+                stk.push("(");
+            } else if(str.charAt(i)=='[') {
+                stk.push("[");
+            } else if(str.charAt(i)==')') {
+                if(stk.empty()) {
+                    flag=true;
+                    break;
+                }
+                int val=0;
+                while(!stk.empty()) {
+                    String top=stk.peek();
+                    if(top.equals(")")||top.equals("]")||top.equals("[")) {
+                        flag=true;
+                        break;
+                    }
+                    if(top.equals("(")) break;
+                    val+=Integer.parseInt(stk.pop());
+                }
+                if(flag) break;
+                if(!stk.empty()&&stk.peek().equals("(")) {
+                    stk.pop();
+                } else {
+                    flag=true;
+                    break;
+                }
+                if(val==0) stk.push("2");
+                else stk.push(String.valueOf(val*2));
+            } else {
+                if(stk.empty()) {
+                    flag=true;
+                    break;
+                }
+                int val=0;
+                while(!stk.empty()) {
+                    String top=stk.peek();
+                    if(top.equals(")")||top.equals("]")||top.equals("(")) {
+                        flag=true;
+                        break;
+                    }
+                    if(top.equals("[")) break;
+                    val+=Integer.parseInt(stk.pop());
+                }
+                if(flag) break;
+                if(!stk.empty()&&stk.peek().equals("[")) {
+                    stk.pop();
+                } else {
+                    flag=true;
+                    break;
+                }
+                if(val==0) stk.push("3");
+                else stk.push(String.valueOf(val*3));
+            }
+        }
+        if(flag) System.out.println(0);
+        else {
+            try {
+                while (!stk.empty()) {
+                    ret += Integer.parseInt(stk.pop());
+                }
+                System.out.println(ret);
+            } catch (Exception e) {
+                System.out.println(0);
+            }
+        }
+    }
+}
