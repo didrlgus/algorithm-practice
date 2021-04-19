8e4b4fe4e5f546b498702f57492a4455dbdafbfe
양기현 <rlgusdid@naver.com>
2021-04-19 21:54:00 +0900

    [programmers] 신규 아이디 추천

--- programmers/level1/s41.cpp
+++ programmers/level1/s41.cpp
@@ -2,48 +2,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 string solution(string new_id) {
-    string answer="";
+    string answer = "";
+    transform(new_id.begin(),new_id.end(),new_id.begin(),::tolower);
+    string tmp_str="";
     for(auto ch:new_id) {
-        if(ch>='A'&&ch<='Z') {
-            answer+=ch+32;
-        } else answer+=ch;
+        if(ch>='a'&&ch<='z') tmp_str+=ch;
+        if(ch>='0'&&ch<='9') tmp_str+=ch;
+        if(ch=='-'||ch=='_'||ch=='.') tmp_str+=ch;
     }
-    string tmp="";
-    for(auto ch:answer) {
-        if(ch>='a'&&ch<='z') tmp+=ch;
-        if(ch>='0'&&ch<='9') tmp+=ch;
-        if(ch=='-'||ch=='_'||ch=='.') tmp+=ch;
-    }
-    answer=tmp;
-    tmp=answer[0];
-    for(int i=1;i<(int)answer.size();i++) {
-        tmp+=answer[i];
-        if(answer[i]=='.') {
-            while(i<(int)answer.size()) {
-                if(answer[++i]!='.') break;    
+    new_id=tmp_str;
+    tmp_str="";
+    for(int i=0;i<(int)new_id.size();i++) {
+        if(new_id[i]=='.') {
+            tmp_str+=new_id[i];
+            for(int j=i+1;j<(int)new_id.size();j++) {
+                if(new_id[j]!='.') {
+                    i=j-1;
+                    break;
+                }
+                if(j==new_id.size()-1) i=j;
             }
-            i--;
-        }
+        } else tmp_str+=new_id[i];
     }
-    answer=tmp;
-    tmp="";
-    while(answer.front()=='.'||answer.back()=='.') {
-        if(answer.front()=='.') answer=answer.substr(1);
-        if(answer.back()=='.') answer=answer.substr(0,(int)answer.size()-1);
+    new_id=tmp_str;
+    if(new_id.front()=='.') new_id=new_id.substr(1);
+    if(new_id.back()=='.') new_id=new_id.substr(0,(int)new_id.size()-1);
+    if(new_id=="") new_id+='a';
+    if((int)new_id.size()>=16) new_id=new_id.substr(0,15);
+    if(new_id.back()=='.') new_id=new_id.substr(0,(int)new_id.size()-1);
+    if((int)new_id.size()<=2) {
+        char last_ch=new_id.back();
+        while(new_id.size()<3) new_id+=last_ch;
     }
-    if(answer=="") answer+="a";
-    if((int)answer.size()>=16) {
-        for(int i=0;i<=14;i++) tmp+=answer[i];
-        answer=tmp;
-    }
-    if(answer.back()=='.') answer=answer.substr(0,(int)answer.size()-1);
-    if((int)answer.size()<=2) {
-        char ch=answer.back();
-        while(answer.size()<3) answer+=ch;
-    }
-    return answer;
+    return new_id;
 }
+
 int main() {
-    cout<<solution("z-+.^.")<<'\n';
+    string new_id="...!@BaT#*..y.ab...b";
+    cout<<solution(new_id)<<'\n';
     return 0;
 }
