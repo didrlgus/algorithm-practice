fa53248646344b7cc8ef2fa6eebaa112c063f700
양기현 <rlgusdid@naver.com>
2021-04-23 12:56:40 +0900

    [programmers] 문자열 압축

--- programmers/level2/s11.cpp
+++ programmers/level2/s11.cpp
@@ -1,58 +1,36 @@
 // 문자열 압축
-#include <string>
-#include <vector>
-#include <cmath>
-
+#include<bits/stdc++.h>
 using namespace std;
-
+int n;
 int solution(string s) {
-    int answer = s.length();
-
-    int size=s.length();
-
-    if(size==1) return 1;
-
-    // s   aabbaccc (8)
-    // i   3
-    // j   2
-    // s1  bac
-    // s2  cc
-    // rs  aabbac
-    // cnt 1 
-    for(int i=1;i<=size/2;i++) {
-        string s1=s.substr(0,i);    // 0번째에서 i개 만큼의 문자열
-        string rs="";
-        int cnt=1;
-
-        for(int j=i;j<size;j+=i) {
-            string s2=s.substr(j,i);
-
-            if(s1==s2) {
-                cnt++;
+    int answer = 0;
+    n=(int)s.size();
+    answer=n;
+    for(int i=1;i<=n/2;i++) {
+        int idx=0,num=0;
+        string str="";
+        string prev_str=s.substr(idx,i);
+        idx+=i;
+        num++;
+        for(int j=idx;j<n;j+=i) {
+            string cur_str=s.substr(j,i);
+            if(prev_str==cur_str) {
+                num++;
             } else {
-                if(cnt<=1) rs+=s1;
-                else rs=rs+to_string(cnt)+s1;
-                s1=s2;
-                cnt=1;
-            }
-
-            if(j+i>=size) {
-                if(cnt<=1) rs+=s.substr(j);
-                else rs=rs+to_string(cnt)+s1;
+                if(num>1) str+=to_string(num)+prev_str;
+                else str+=prev_str;
+                prev_str=cur_str;
+                num=1;
             }
         }
-        int len=rs.length();
-        answer=min(answer,len);
+        if(num>1) str+=to_string(num)+prev_str;
+        else str+=prev_str;
+        answer=min(answer,(int)str.size());
     }
-
     return answer;
 }
-
 int main() {
-    string s="aabbaccc";
-    int result=solution(s);
-
-    printf("%d\n",result);
-
+    string s="abcabcabcabcdededededede";
+    printf("%d\n",solution(s));
     return 0;
 }
