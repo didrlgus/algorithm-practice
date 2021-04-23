42855f89ff05aa078a0c90ea85722e09e30c6145
양기현 <rlgusdid@naver.com>
2021-04-23 11:47:03 +0900

    [programmers] 다트게임

--- programmers/level1/s31.cpp
+++ programmers/level1/s31.cpp
@@ -1,55 +1,45 @@
 // 다트게임
-#include <string>
-#include <vector>
-#include <cmath>
+#include<bits/stdc++.h>
 using namespace std;
-
+vector<int> v;
 int solution(string dartResult) {
-    int answer = 0;
-
-    vector<int> v;
-    int idx=0;
-
-    for(int i=0;i<dartResult.size();i++) {
-        char c=dartResult.at(i);
-
-        if(c=='S'){}
-        else if(c=='D') {
-            v[idx-1]=pow(v[idx-1],2);
-        } else if(c=='T') {
-            v[idx-1]=pow(v[idx-1],3);
-        } else if(c=='*') {
-            if(idx==1) {
-                v[idx-1]*=2;
-            } else {
-                v[idx-2]*=2;
-                v[idx-1]*=2;
-            }
-        } else if(c=='#') {
-            v[idx-1]*=-1;
-        } else {
-            if(c=='1' && dartResult.at(i+1)=='0') {
-                v.push_back(10);
+    int answer=0;
+    for(int i=0;i<(int)dartResult.size();i++) {
+        if(dartResult[i]>='0'&&dartResult[i]<='9') {
+            string num_str="";
+            while(dartResult[i]>='0'&&dartResult[i]<='9') {
+                num_str+=dartResult[i];
                 i++;
-            } else {
-                v.push_back(c-'0');
             }
-            idx++;
+            v.push_back(stoi(num_str.c_str()));
+            i--;
+            continue;
+        }
+        if(dartResult[i]=='S'||dartResult[i]=='D'||dartResult[i]=='T') {
+            if(dartResult[i]=='D') v.back()=pow(v.back(),2);
+            if(dartResult[i]=='T') v.back()=pow(v.back(),3);
+            continue;
+        }
+        if(dartResult[i]=='*'||dartResult[i]=='#') {
+            int len=(int)v.size();
+            if(dartResult[i]=='*') {
+                if(len==1) {
+                    v[len-1]*=2;
+                } else {
+                    v[len-1]*=2;
+                    v[len-2]*=2;
+                }
+            } else {
+                v[len-1]*=-1;
+            }
         }
     }
-
-    for(int i=0;i<v.size();i++) {
-        answer+=v[i];
-    }
+    for(auto it:v) answer+=it;
     return answer;
 }
-
 int main() {
     string dartResult="1S2D*3T";
-
     int result=solution(dartResult);
-
     printf("%d\n",result);
-
     return 0;
 }
