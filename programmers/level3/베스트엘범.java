60edcba07a3b317e018932696b3dfe960ef25839
didrlgus <rlgusdid@naver.com>
2021-11-01 23:06:41 +0900

    [programmers] 베스트엘범

--- programmers/level3/베스트엘범.java
+++ programmers/level3/베스트엘범.java
@@ -1,77 +1,87 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Data {
-
-    int play;
-    int uniqueNumber;
-
-    public Data(int play, int uniqueNumber) {
+    
+    Integer num;
+    Integer play;
+    
+    public Data(Integer num, Integer play) {
+        this.num = num;
         this.play = play;
-        this.uniqueNumber = uniqueNumber;
     }
+}
 
+class GenrePlays {
+    
+    String genre;
+    Integer play;
+    
+    public GenrePlays(String genre, Integer play) {
+        this.genre = genre;
+        this.play = play;
+    }
 }
 
 class Solution {
     
-    Map<String, Integer> map1 = new HashMap<>();
-    Map<String, List<Data>> map2 = new HashMap<>();
-
+    private Map<String, Integer> genreMap = new HashMap<>();
+    private Map<String, List<Data>> sortedMap = new HashMap<>();
+    
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer;
-
-        List<Integer> ret = new ArrayList<>();
-
+        int[] answer = {};
+        
         int N = genres.length;
-
+        
         for (int i = 0; i < N; i++) {
             String genre = genres[i];
             int play = plays[i];
-
-            if (!map1.containsKey(genre)) {
-                map1.put(genre, play);
+            if (!genreMap.containsKey(genre)) {
+                genreMap.put(genre, play);
             } else {
-                map1.put(genre, map1.get(genre) + play);
+                genreMap.put(genre, genreMap.get(genre) + play);
             }
-
-            List<Data> list;
-            if (!map2.containsKey(genre)) {
-                list = new ArrayList<>();
+            if (!sortedMap.containsKey(genre)) {
+                List<Data> list = new ArrayList<>();
+                list.add(new Data(i, play));
+                sortedMap.put(genre, list);
             } else {
-                list = map2.get(genre);
-            }
-            list.add(new Data(play , i));
-            map2.put(genre, list);
-        }
-
-        List<Map.Entry<String, Integer>> collect
-                = map1.entrySet().stream().sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue())).collect(Collectors.toList());
-
-        for (Map.Entry<String, Integer> entry : collect) {
-            String key = entry.getKey();
-
-            List<Data> dataList = map2.get(key);
-
-            List<Data> sortedList = dataList.stream().sorted((o1, o2) -> {
-                if (o1.play == o2.play) {
-                    return o1.uniqueNumber - o2.uniqueNumber;
-                }
-                return o2.play - o1.play;
-            }).collect(Collectors.toList());
-
-            ret.add(sortedList.get(0).uniqueNumber);
-            if (sortedList.size() >= 2) {
-                ret.add(sortedList.get(1).uniqueNumber);
+                List<Data> tmpList = sortedMap.get(genre);
+                tmpList.add(new Data(i, play));
+                sortedMap.put(genre, tmpList);
             }
         }
-
+        
+        List<GenrePlays> genreRank = new ArrayList<>();
+        
+        for (Map.Entry<String, Integer> entry : genreMap.entrySet()) {
+            genreRank.add(new GenrePlays(entry.getKey(), entry.getValue()));
+        }
+        
+        genreRank.sort((o1, o2) -> o2.play.compareTo(o1.play));
+        
+        genreRank.forEach(o -> {
+            List<Data> tmpList = sortedMap.get(o.genre);
+            tmpList.sort((o1, o2) -> o2.play.compareTo(o1.play));
+        });
+        
+        List<Integer> ret = new ArrayList<>();
+        
+        genreRank.forEach(o -> {
+            List<Data> tmpList = sortedMap.get(o.genre);
+            
+            int M = tmpList.size() < 2 ? tmpList.size() : 2;
+            
+            for (int i = 0; i < M; i++) {
+                ret.add(tmpList.get(i).num);
+            }
+        });
+        
         answer = new int[ret.size()];
-
-        for (int i = 0 ; i < ret.size(); i++) {
+        
+        for (int i = 0; i < ret.size(); i++) {
             answer[i] = ret.get(i);
         }
-
+        
         return answer;
     }
 }
